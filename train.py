16c3cadd9670de2444a1c1f546ebc86cc4f088dc
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-03 22:49:36 +0200

    Added gan slurm scripts.

--- train.py
+++ train.py
@@ -30,14 +30,14 @@ py.arg('--load_size', type=int, default=520)  # load image to this size
 py.arg('--crop_size', type=int, default=512)  # then crop to this size
 py.arg('--batch_size', type=int, default=1)
 py.arg('--epochs', type=int, default=20)
-py.arg('--epoch_decay', type=int, default=100)  # epoch to start decaying learning rate
+py.arg('--epoch_decay', type=int, default=8)  # epoch to start decaying learning rate
 py.arg('--lr', type=float, default=0.0002)
 py.arg('--beta_1', type=float, default=0.5)
 py.arg('--adversarial_loss_mode', default='lsgan', choices=['gan', 'hinge_v1', 'hinge_v2', 'lsgan', 'wgan'])
 py.arg('--gradient_penalty_mode', default='none', choices=['none', 'dragan', 'wgan-gp'])
 py.arg('--gradient_penalty_weight', type=float, default=10.0)
-py.arg('--cycle_loss_weight', type=float, default=10.0)
-py.arg('--counterfactual_loss_weight', type=float, default=5.0)
+py.arg('--cycle_loss_weight', type=float, default=1)
+py.arg('--counterfactual_loss_weight', type=float, default=1)
 py.arg('--identity_loss_weight', type=float, default=0.0)
 py.arg('--pool_size', type=int, default=50)  # pool size to store fake samples
 args = py.args()
