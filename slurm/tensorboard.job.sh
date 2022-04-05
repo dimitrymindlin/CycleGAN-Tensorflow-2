a30f09b609af539a1e4b8455e5f5248586053166
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-05 14:02:16 +0200

    Corrected path for tensorboard slurm.

--- slurm/tensorboard.job.sh
+++ slurm/tensorboard.job.sh
@@ -2,7 +2,7 @@
 source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 
-cd /media/compute/homes/dmindlin/medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 tensorboard dev upload --logdir output/apple2orange/summaries/train \
     --name "AppleOrange Attention" \
