1592aa144ae162bb1b98ee4cfc8c2e5c3a243ffc
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-05 22:57:39 +0200

    Added mura dataset, training, and picture logging.

--- train.py
+++ train.py
@@ -1,4 +1,10 @@
 import functools
+import json
+import os
+from datetime import datetime
+
+from matplotlib import pyplot as plt
+
 import attention_maps
 import imlib as im
 import pylib as py
@@ -19,7 +25,7 @@ import module
 # =                                   param                                    =
 # ==============================================================================
 
-py.arg('--dataset', default='apple2orange')
+py.arg('--dataset', default='mura')
 py.arg('--datasets_dir', default='datasets')
 py.arg('--load_size', type=int, default=520)  # load image to this size
 py.arg('--crop_size', type=int, default=512)  # then crop to this size
@@ -38,8 +44,10 @@ py.arg('--pool_size', type=int, default=50)  # pool size to store fake samples
 py.arg('--attention', type=str, default="gradcam")
 args = py.args()
 
+execution_id = datetime.now().strftime("%Y-%m-%d--%H.%M")
+
 # output_dir
-output_dir = py.join(f'output_{args.attention}', args.dataset)
+output_dir = py.join(f'output_{args.dataset}/{execution_id}')
 py.mkdir(output_dir)
 
 # save settings
@@ -48,17 +56,24 @@ py.args_to_yaml(py.join(output_dir, 'settings.yml'), args)
 # ==============================================================================
 # =                                    data                                    =
 # ==============================================================================
+if args.dataset == "mura":
+    # A = 0 = negative, B = 1 = positive
+    train_x, train_y, valid_x, valid_y, test_x, test_y = data.get_mura_data_paths()
+    A_img_paths = [filename for filename in train_x if "negative" in filename]
+    B_img_paths = [filename for filename in train_x if "positive" in filename]
+    A_img_paths_test = [filename for filename in train_x if "negative" in filename]
+    B_img_paths_test = [filename for filename in train_x if "positive" in filename]
+else:
+    A_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainA'), '*.jpg')
+    B_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainB'), '*.jpg')
+    A_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testA'), '*.jpg')
+    B_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testB'), '*.jpg')
 
-A_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainA'), '*.jpg')
-B_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainB'), '*.jpg')
 A_B_dataset, len_dataset = data.make_zip_dataset(A_img_paths, B_img_paths, args.batch_size, args.load_size,
                                                  args.crop_size, training=True, repeat=False)
 
 A2B_pool = data.ItemPool(args.pool_size)
 B2A_pool = data.ItemPool(args.pool_size)
-
-A_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testA'), '*.jpg')
-B_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testB'), '*.jpg')
 A_B_dataset_test, _ = data.make_zip_dataset(A_img_paths_test, B_img_paths_test, args.batch_size, args.load_size,
                                             args.crop_size, training=False, repeat=True)
 
@@ -75,13 +90,19 @@ D_B = module.ConvDiscriminator(input_shape=(args.crop_size, args.crop_size, 3))
 d_loss_fn, g_loss_fn = gan.get_adversarial_losses_fn(args.adversarial_loss_mode)
 cycle_loss_fn = tf.losses.MeanAbsoluteError()
 identity_loss_fn = tf.losses.MeanAbsoluteError()
+counterfactual_loss_nf = tf.losses.MeanSquaredError()
 
-clf = tf.keras.models.load_model(f"checkpoints/inception_apple_orange/model", compile=False)
+if args.dataset == "mura":
+    clf = tf.keras.models.load_model(f"checkpoints/2022-03-24--12.42/model", compile=False)
+else:
+    clf = tf.keras.models.load_model(f"checkpoints/inception_apple_orange/model", compile=False)
 
 G_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
 D_lr_scheduler = module.LinearDecay(args.lr, args.epochs * len_dataset, args.epoch_decay * len_dataset)
 G_optimizer = keras.optimizers.Adam(learning_rate=G_lr_scheduler, beta_1=args.beta_1)
 D_optimizer = keras.optimizers.Adam(learning_rate=D_lr_scheduler, beta_1=args.beta_1)
+class_A_ground_truth = np.stack([np.ones(args.batch_size), np.zeros(args.batch_size)]).T
+class_B_ground_truth = np.stack([np.zeros(args.batch_size), np.ones(args.batch_size)]).T
 
 
 # ==============================================================================
@@ -108,8 +129,8 @@ def train_G(A, B):
         A2A_id_loss = identity_loss_fn(A, A2A)
         B2B_id_loss = identity_loss_fn(B, B2B)
 
-        A2B_counterfactual_loss = clf(A2B)
-        B2A_counterfactual_loss = clf(B2A)
+        A2B_counterfactual_loss = counterfactual_loss_nf(class_B_ground_truth, clf(A2B))
+        B2A_counterfactual_loss = counterfactual_loss_nf(class_A_ground_truth, clf(B2A))
 
         G_loss = (A2B_g_loss + B2A_g_loss) + (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight \
                  + (A2A_id_loss + B2B_id_loss) * args.identity_loss_weight + \
@@ -196,11 +217,11 @@ except Exception as e:
     print(e)
 
 # summary
-train_summary_writer = tf.summary.create_file_writer(py.join(output_dir, 'summaries', 'train'))
+train_summary_writer = tf.summary.create_file_writer(py.join(f'logs/' + execution_id))
 
 # sample
 test_iter = iter(A_B_dataset_test)
-sample_dir = py.join(output_dir, 'samples_training_mul')
+sample_dir = py.join(output_dir, 'images')
 py.mkdir(sample_dir)
 
 # Create GradCAM++ object
@@ -219,17 +240,12 @@ with train_summary_writer.as_default():
         ep_cnt.assign_add(1)
 
         # train for an epoch
+        batch_count = 0
         for A, B in tqdm.tqdm(A_B_dataset, desc='Inner Epoch Loop', total=len_dataset):
             A, _ = attention_maps.get_gradcam(A, gradcam, clf, 0)
             B, _ = attention_maps.get_gradcam(B, gradcam, clf, 1)
 
             G_loss_dict, D_loss_dict = train_step(A, B)
-
-            # # summary
-            tl.summary(G_loss_dict, step=G_optimizer.iterations, name='G_losses')
-            tl.summary(D_loss_dict, step=G_optimizer.iterations, name='D_losses')
-            tl.summary({'learning rate': G_lr_scheduler.current_learning_rate}, step=G_optimizer.iterations,
-                       name='learning rate')
 
             # sample
             if G_optimizer.iterations.numpy() % 100 == 0:
@@ -240,9 +256,47 @@ with train_summary_writer.as_default():
                 B_attention, B_heatmap = attention_maps.get_gradcam(B, gradcam, clf, 1)
 
                 A2B, B2A, = sample(A_attention, B_attention)
-                img = im.immerge(np.concatenate([A, A2B, A_heatmap, A_attention, B, B2A, B_heatmap, B_attention], axis=0),
-                                 n_rows=2)
-                im.imwrite(img, py.join(sample_dir, 'iter-%09d.jpg' % G_optimizer.iterations.numpy()))
+                """img = im.immerge(
+                    np.concatenate([A, A2B, A_heatmap, A_attention, B, B2A, B_heatmap, B_attention], axis=0),
+                    n_rows=2)
+                im.imwrite(img, py.join(sample_dir, 'iter-%09d.jpg' % G_optimizer.iterations.numpy()))"""
+                r, c = 2, 4
+
+                titles = ['Original', 'Attention', 'Input Image', 'Translated']
+                imgs = [A, A_heatmap, A_attention, A2B, B, B_heatmap, B_attention, B2A]
+
+                classification = [['Normal', 'Abnormal'][int(np.argmax(clf.predict(x)))] for x in
+                                  [A, A_heatmap, A_attention, A2B, B, B_heatmap, B_attention, B2A]]
+
+                gen_imgs = np.concatenate(imgs)
+                correct_classification = ['Normal', 'Normal', 'Normal', 'Abnormal',
+                                          'Abnormal', 'Abnormal', 'Abnormal', 'Normal']
+
+
+                fig, axs = plt.subplots(r, c, figsize=(15, 10))
+                cnt = 0
+                for i in range(r):
+                    for j in range(c):
+                        axs[i, j].imshow(gen_imgs[cnt][:, :, 0])
+                        if j >= 1:
+                            axs[i, j].set_title(
+                                f'{titles[j]} T: ({correct_classification[cnt]} | P: {classification[cnt]})')
+                        else:
+                            axs[i, j].set_title(f'{titles[j]}')
+                        axs[i, j].axis('off')
+                        cnt += 1
+
+                img_folder = f'output_{args.dataset}/{execution_id}/images'
+                os.makedirs(img_folder, exist_ok=True)
+                fig.savefig(f"{img_folder}/%d_%d.png" % (ep_cnt, batch_count))
+                plt.close()
+            batch_count += 1
+
+        # # summary
+        tl.summary(G_loss_dict, step=G_optimizer.iterations, name='G_losses')
+        tl.summary(D_loss_dict, step=G_optimizer.iterations, name='D_losses')
+        tl.summary({'learning rate': G_lr_scheduler.current_learning_rate}, step=G_optimizer.iterations,
+                   name='learning rate')
 
         # save checkpoint
         checkpoint.save(ep)
