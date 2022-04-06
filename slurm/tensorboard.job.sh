74273c58bd4caebe578228dda009a45c2230ecb1
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-06 17:00:06 +0200

    corrected tensorboard path.

--- slurm/tensorboard.job.sh
+++ slurm/tensorboard.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-tensorboard dev upload --logdir output/apple2orange/summaries/train \
-    --name "AppleOrange Attention" \
+tensorboard dev upload --logdir logs \
+    --name "Mura Attention Gan" \
     --description "AppleOrange Attention" \
     --one_shot
