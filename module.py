d73289c443fb7f3e2468fa992ccdd7f32cfe700a
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-09 14:51:27 +0200

    Added Unet Generator model

--- module.py
+++ module.py
@@ -1,11 +1,15 @@
 import tensorflow as tf
 import tensorflow_addons as tfa
 import tensorflow.keras as keras
-
+from keras import Input, Model
+from keras.layers import Conv2D, ReLU, LeakyReLU, UpSampling2D, Dropout, Concatenate
+from tensorflow_addons.layers import InstanceNormalization
 
 # ==============================================================================
 # =                                  networks                                  =
 # ==============================================================================
+
+
 
 def _get_norm_layer(norm):
     if norm == 'none':
@@ -20,7 +24,7 @@ def _get_norm_layer(norm):
 
 def ResnetGenerator(input_shape=(256, 256, 3),
                     output_channels=3,
-                    dim=64,
+                    filters=64,
                     n_downsamplings=2,
                     n_blocks=9,
                     norm='instance_norm'):
@@ -46,14 +50,14 @@ def ResnetGenerator(input_shape=(256, 256, 3),
 
     # 1
     h = tf.pad(h, [[0, 0], [3, 3], [3, 3], [0, 0]], mode='REFLECT')
-    h = keras.layers.Conv2D(dim, 7, padding='valid', use_bias=False)(h)
+    h = keras.layers.Conv2D(filters, 7, padding='valid', use_bias=False)(h)
     h = Norm()(h)
     h = tf.nn.relu(h)
 
     # 2
     for _ in range(n_downsamplings):
-        dim *= 2
-        h = keras.layers.Conv2D(dim, 3, strides=2, padding='same', use_bias=False)(h)
+        filters *= 2
+        h = keras.layers.Conv2D(filters, 3, strides=2, padding='same', use_bias=False)(h)
         h = Norm()(h)
         h = tf.nn.relu(h)
 
@@ -63,8 +67,8 @@ def ResnetGenerator(input_shape=(256, 256, 3),
 
     # 4
     for _ in range(n_downsamplings):
-        dim //= 2
-        h = keras.layers.Conv2DTranspose(dim, 3, strides=2, padding='same', use_bias=False)(h)
+        filters //= 2
+        h = keras.layers.Conv2DTranspose(filters, 3, strides=2, padding='same', use_bias=False)(h)
         h = Norm()(h)
         h = tf.nn.relu(h)
 
@@ -74,6 +78,52 @@ def ResnetGenerator(input_shape=(256, 256, 3),
     h = tf.tanh(h)
 
     return keras.Model(inputs=inputs, outputs=h)
+
+def UnetGenerator(input_shape, filters=32, channels=3, leaky_relu=False, skip_connections=True):
+    """U-Net Generator"""
+
+    def conv2d(layer_input, filters, f_size=4, last=False):
+        """Layers used during downsampling"""
+        d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
+        if last:
+            d = ReLU()(d)
+            return d
+        if not leaky_relu:
+            d = ReLU()(
+                d)  # TODO: Try Normal Relu https://machinelearningmastery.com/how-to-train-stable-generative-adversarial-networks/
+        else:
+            d = LeakyReLU(alpha=0.2)(d)
+        d = InstanceNormalization()(d, training=True) # https://machinelearningmastery.com/how-to-implement-pix2pix-gan-models-from-scratch-with-keras/
+        return d
+
+    def deconv2d(layer_input, skip_input, filters, f_size=4, dropout_rate=0):
+        """Layers used during upsampling"""
+        u = UpSampling2D(size=2)(layer_input)
+        u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
+        if dropout_rate:
+            u = Dropout(dropout_rate)(u)
+        u = InstanceNormalization()(u)
+        if skip_connections:
+            u = Concatenate()([u, skip_input])
+        return u
+
+    # Image input
+    d0 = Input(shape=input_shape)
+    # Downsampling
+    d1 = conv2d(d0, filters)
+    d2 = conv2d(d1, filters * 2)
+    d3 = conv2d(d2, filters * 4)
+    d4 = conv2d(d3, filters * 8, last=True)
+
+    # Upsampling
+    u1 = deconv2d(d4, d3, filters * 4)
+    u2 = deconv2d(u1, d2, filters * 2)
+    u3 = deconv2d(u2, d1, filters)
+
+    u4 = UpSampling2D(size=2)(u3)
+    output_img = Conv2D(channels, kernel_size=4, strides=1, padding='same', activation='tanh')(u4)
+
+    return Model(d0, output_img)
 
 
 def ConvDiscriminator(input_shape=(256, 256, 3),
