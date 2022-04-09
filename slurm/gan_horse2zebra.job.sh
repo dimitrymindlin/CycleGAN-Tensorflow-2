e3a5183ce129f07ad275d726b6191518512479af
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-09 14:37:40 +0200

    Adapted intensity.

--- slurm/gan_horse2zebra.job.sh
+++ slurm/gan_horse2zebra.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset horse2zebra --attention_intensity 0.5
+python3 -m train --dataset horse2zebra --attention_intensity 0.8
