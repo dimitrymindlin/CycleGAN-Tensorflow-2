a76953ff0ad896ff65ab5c76f69082bf7d172205
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-09 14:56:31 +0200

    Adapetd intensity.

--- slurm/gan_horse2zebra_unet.job.sh
+++ slurm/gan_horse2zebra_unet.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset horse2zebra --attention_intensity 0.2 --generator unet
+python3 -m train --dataset horse2zebra --attention_intensity 0.5 --generator unet
