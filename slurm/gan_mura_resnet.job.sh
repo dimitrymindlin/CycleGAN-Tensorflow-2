a445186225f572f3228297dc21412c1064e9918c
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-14 15:58:01 +0200

    increased cycle-consistency weight.

--- slurm/gan_mura_resnet.job.sh
+++ slurm/gan_mura_resnet.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset mura --generator resnet --attention_type spagan
+python3 -m train --dataset mura --generator resnet --attention_type attention-gan --cycle_loss_weight 10
