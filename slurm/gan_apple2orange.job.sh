df0eddcb740034b7cf146da9348620f0fbd18b1c
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-15 01:21:31 +0200

    adapted settings.

--- slurm/gan_apple2orange.job.sh
+++ slurm/gan_apple2orange.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset apple2orange --generator unet --attention_type attention-gan
+python3 -m train --dataset apple2orange --generator resnet --attention_type attention-gan
