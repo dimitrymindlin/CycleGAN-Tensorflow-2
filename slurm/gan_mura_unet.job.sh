d1c77ed9a69355a7d0c6eb5320dc56b17e7823d4
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-14 16:39:25 +0200

    changed params.

--- slurm/gan_mura_unet.job.sh
+++ slurm/gan_mura_unet.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset mura --generator unet --attention_type spagan
+python3 -m train --dataset mura --generator unet --attention_type spagan --gradient_penalty_weight 10
