2145dfaacd59ebb50d4b4b09831ce022fecfb3cf
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-15 14:19:27 +0200

    added apple to orange normal.

---
+++ slurm/gan_apple2orange_normal.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train --dataset apple2orange --generator resnet cycle_loss_weight 10 --counterfactual_loss_weight 0
