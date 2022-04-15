1b5f5dd332e7065620281ebdee1edc69f1a417fd
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-15 14:18:03 +0200

    Normal slurm.

---
+++ slurm/gan_horse2zebra_normal.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train --dataset horse2zebra --generator resnet --cycle_loss_weight 10 --counterfactual_loss_weight 0
