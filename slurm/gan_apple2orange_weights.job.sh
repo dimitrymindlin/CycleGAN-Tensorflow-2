d7f5295d49bad1e3b61b3c0d7ea704017ad9c8f2
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-18 14:21:48 +0200

    changed settings.

--- slurm/gan_apple2orange_weights.job.sh
+++ slurm/gan_apple2orange_weights.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset apple2orange --generator resnet --attention_type attention-gan --cycle_loss_weight 10
+python3 -m train --dataset apple2orange --generator resnet --gradient_penalty_weight 5
