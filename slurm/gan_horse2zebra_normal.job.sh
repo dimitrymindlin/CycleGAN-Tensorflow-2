170331c80c4873e1dfe053ef982b65381cb0c5ec
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-18 14:25:45 +0200

    changed settings.

--- slurm/gan_horse2zebra_normal.job.sh
+++ slurm/gan_horse2zebra_normal.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset horse2zebra --generator resnet --counterfactual_loss_weight 0
+python3 -m train --dataset horse2zebra --generator unet --counterfactual_loss_weight 0
