a7d8be9530adca9b57a93820c18b68816d48b749
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-21 22:31:22 +0200

    added mura spa_gan.

---
+++ slurm/gan_mura_resnet_spa_gan.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train --dataset mura --generator resnet --attention_type spa-gan --attention_intensity 0.3 --cycle_loss_weight 5 --gradient_penalty_weight 5
