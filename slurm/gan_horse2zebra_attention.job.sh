bb02d148b034e2c971350410fdedc71fa5ad2fa4
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-21 15:26:09 +0200

    Added new slurm for original attention.

---
+++ slurm/gan_horse2zebra_attention.job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train --dataset horse2zebra --generator resnet --attention_type attention-gan --cycle_loss_weight 5 --gradient_penalty_weight 5 --attention_gan_original True
