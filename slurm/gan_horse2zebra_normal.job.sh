830013fa2d0b0b86a33226971024e15aa504f349
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-24 11:48:43 +0200

    Added counterfactual loss.

--- slurm/gan_horse2zebra_normal.job.sh
+++ slurm/gan_horse2zebra_normal.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset horse2zebra --generator resnet --counterfactual_loss_weight 0 --gradient_penalty_weight 10 --cycle_loss_weight 10
+python3 -m train --dataset horse2zebra --generator resnet --counterfactual_loss_weight 1 --gradient_penalty_weight 10 --cycle_loss_weight 10
