3f01ac6161ec38698cb40be9d18e5918bad1765b
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-24 22:21:52 +0200

    Adapted slurm and epochs.

--- slurm/gan_horse2zebra_attention.job.sh
+++ slurm/gan_horse2zebra_attention.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset horse2zebra --generator resnet --attention_type attention-gan --cycle_loss_weight 10 --gradient_penalty_weight 10 --attention_gan_original True
+python3 -m train --dataset horse2zebra --generator resnet --attention_type attention-gan --cycle_loss_weight 10 --gradient_penalty_weight 10 --attention_gan_original True --counterfactual_loss_weight 0
