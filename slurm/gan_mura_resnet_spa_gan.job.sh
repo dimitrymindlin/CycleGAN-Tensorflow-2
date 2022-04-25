202454efd083df2a352b60110e6940fa11327784
dimi9595 <dmitry.mindlin@gmail.com>
2022-04-25 12:50:54 +0200

    chages slurm settings.

--- slurm/gan_mura_resnet_spa_gan.job.sh
+++ slurm/gan_mura_resnet_spa_gan.job.sh
@@ -3,4 +3,4 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train --dataset mura --generator resnet --attention_type spa-gan --attention_intensity 0.5 --cycle_loss_weight 5 --gradient_penalty_weight 10
+python3 -m train --dataset mura --generator resnet --attention_type spa-gan --attention_intensity 0.5 --cycle_loss_weight 1 --gradient_penalty_weight 5
