5b9ab4187b9b4f6dfbeb39f61a0f40803344f962
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-03 19:07:11 +0200

    changed settings.

--- slurm/gan_horse2zebra_normal.job.sh
+++ slurm/gan_horse2zebra_normal.job.sh
@@ -6,8 +6,10 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 python3 -m train \
   --dataset horse2zebra \
   --generator resnet \
+  --epochs 100 \
+  --epoch_decay 50 \
   --attention_type none \
   --attention_gan_original False \
-  --discriminator_loss_weight 5 \
+  --discriminator_loss_weight 10 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0
