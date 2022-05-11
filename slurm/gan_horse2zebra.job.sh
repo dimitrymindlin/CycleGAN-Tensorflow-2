71b61e110e061ee53fe9389d05901cb9339c8702
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-11 17:41:29 +0200

    trying unet.

--- slurm/gan_horse2zebra.job.sh
+++ slurm/gan_horse2zebra.job.sh
@@ -5,8 +5,8 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train \
   --dataset horse2zebra \
-  --generator resnet \
-  --attention_type attention-gan-original \
-  --discriminator_loss_weight 10 \
-  --cycle_loss_weight 1 \
+  --generator unet \
+  --attention_type none \
+  --discriminator_loss_weight 1 \
+  --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0
