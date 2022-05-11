422acdd389a3391d4f5bc76a2ce069dd9ed7ca8f
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-11 17:39:37 +0200

    new settings with 200 epochs and attention.

--- slurm/gan_horse2zebra_attention.job.sh
+++ slurm/gan_horse2zebra_attention.job.sh
@@ -6,7 +6,9 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 python3 -m train \
   --dataset horse2zebra \
   --generator resnet \
-  --attention_type spa-gan \
+  --epochs 200 \
+  --epoch_decay 100 \
+  --attention_type attention-gan-original \
   --discriminator_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0
