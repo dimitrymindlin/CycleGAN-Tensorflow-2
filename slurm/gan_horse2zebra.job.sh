823d122f7396178e69bfff774533fa4cb8cde2bd
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-12 16:28:06 +0200

    changed settings.

--- slurm/gan_horse2zebra.job.sh
+++ slurm/gan_horse2zebra.job.sh
@@ -5,8 +5,8 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train \
   --dataset horse2zebra \
-  --generator unet \
-  --attention_type none \
-  --discriminator_loss_weight 1 \
+  --generator resnet \
+  --attention_type spa-gan \
+  --discriminator_loss_weight 5 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0
