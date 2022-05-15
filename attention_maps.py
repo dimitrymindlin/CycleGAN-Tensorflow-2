733881d750e693313b52eb84925a58f90c6be900
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-15 15:38:04 +0200

    added attention_maps and img holder

---
+++ attention_maps.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+from tf_keras_vis.utils.scores import CategoricalScore
+
+from imlib import scale_to_zero_one, scale_to_minus_one_one
+
+
+def shift_values_above_intensity(cam, attention_intensity: float):
+    """
+    makes sure all values are above attention_intensity value.
+    """
+    cam += attention_intensity
+    cam /= np.max(cam)
+    return cam
+
+
+def apply_gradcam(img, gradcam, class_index, attention_type, attention_intensity=1):
+    """
+    Applys gradcam to an image and returns the heatmap as well as the enhanced img.
+    """
+    # Generate cam map
+    cam = gradcam(CategoricalScore(class_index), img)  # returns ndarray in [0,1]
+    if np.max(cam) == 0 and np.min(cam) == 0:
+        cam += 1
+    # Turn to batched 3-channel array
+    cam = tf.expand_dims(cam, axis=-1)
+    cam = tf.image.grayscale_to_rgb(tf.convert_to_tensor(cam))
+    # Convert img to same pixel values [0, 1]
+    img = scale_to_zero_one(img)
+
+    if attention_type == "spa-gan":
+        cam = shift_values_above_intensity(cam, attention_intensity)
+    cam = tf.ones(shape=cam.shape) # TODO: Delete, only for testing
+
+    # Interpolate by multiplication and normalise
+    img = cam * img
+    img /= np.max(img)
+    return scale_to_minus_one_one(img), scale_to_minus_one_one(cam)  # [-1,1]
