e60e75cca6f141b234c5599827b8ebab533ea5a1
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-15 23:56:37 +0200

    New settings.

--- slurm/gan_horse2zebra_feature_map_loss.job.sh
+++ slurm/gan_horse2zebra_feature_map_loss.job.sh
@@ -6,10 +6,4 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 python3 -m train \
   --dataset horse2zebra \
   --generator resnet \
-  --epochs 70 \
-  --epoch_decay 50 \
-  --attention_type spa-gan \
-  --discriminator_loss_weight 1 \
-  --cycle_loss_weight 10 \
-  --counterfactual_loss_weight 0 \
-  --feature_map_loss_weight 1
+  --attention_type attention-gan-original \
--- slurm/gan_horse2zebra_unet.job.sh
+++ slurm/gan_horse2zebra_unet.job.sh
@@ -7,6 +7,3 @@ python3 -m train \
   --dataset horse2zebra \
   --generator resnet \
   --attention_type spa-gan \
-  --discriminator_loss_weight 1 \
-  --cycle_loss_weight 10 \
-  --counterfactual_loss_weight 0 \
