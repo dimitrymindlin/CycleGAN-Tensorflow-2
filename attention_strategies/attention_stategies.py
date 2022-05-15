9468f2f4acf16f4432958fe5e0e0bf9a7eb9d2aa
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-15 16:16:28 +0200

    Revert "removed img holder."
    This reverts commit 764c34197132cad0bc08c0dfa9b49ecaedf8b83c.

--- attention_strategies/attention_stategies.py
+++ attention_strategies/attention_stategies.py
@@ -79,8 +79,8 @@ def spa_gan(A, B, G_A2B, G_B2A, training=True):
         return A2B, B2A
 
 def no_attention(A, B, G_A2B, G_B2A, training=True):
-    A2B = G_A2B(A, training=training)
-    B2A = G_B2A(B, training=training)
+    A2B = G_A2B(A.img, training=training)
+    B2A = G_B2A(B.img, training=training)
     if training:
         # Cycle
         A2B2A = G_B2A(A2B, training=training)
