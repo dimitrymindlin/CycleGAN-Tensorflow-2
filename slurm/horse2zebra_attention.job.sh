8a4eecbb3febd94f1976c62aac78f6b2a36d8ad0
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-16 22:34:37 +0200

    added new slurm job.

---
+++ slurm/horse2zebra_attention.job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train \
+  --dataset horse2zebra \
+  --attention_type attention-gan-original \
+
+
