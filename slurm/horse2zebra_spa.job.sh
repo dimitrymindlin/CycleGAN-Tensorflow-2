4a80c34bb31bb28ffdf710d9fd9f559c6a8b1ea8
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-17 17:24:28 +0200

    Baseline to make spa-gan insisible.

--- slurm/horse2zebra_spa.job.sh
+++ slurm/horse2zebra_spa.job.sh
@@ -6,5 +6,7 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 python3 -m train_spa_gan \
   --dataset horse2zebra \
   --generator resnet \
+  --feature_map_loss_weight 0 \
+  --attention_intensity 0
 
 
