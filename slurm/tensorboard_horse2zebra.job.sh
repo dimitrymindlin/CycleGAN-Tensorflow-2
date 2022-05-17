0014059a103c5d55a480260fd6b966764b54f4ab
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-17 11:09:07 +0200

    Adapted logs to chosen path.

--- slurm/tensorboard_horse2zebra.job.sh
+++ slurm/tensorboard_horse2zebra.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-tensorboard dev upload --logdir logs/horse2zebra \
-    --name "Horse2Zebra find-problem" \
+tensorboard dev upload --logdir logs/horse2zebra/chosen \
+    --name "Horse2Zebra find-problem chosen" \
     --description "" \
     --one_shot
