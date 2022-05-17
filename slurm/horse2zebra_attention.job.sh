66e320363cddb3142a0ecd728b79b2efa7996266
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-17 17:39:16 +0200

    fixed slurms.

--- slurm/horse2zebra_attention.job.sh
+++ slurm/horse2zebra_attention.job.sh
@@ -3,7 +3,7 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train \
+python3 -m train_spa_gan \
   --dataset horse2zebra \
   --attention_type spa-gan \
   --feature_map_loss_weight 1 \
