d68e59f23d03ea3008405306a4bf669a716bfccf
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-18 11:48:09 +0200

    Trying counterfactual generation.

---
+++ slurm/horse2zebra_spa_counterfactual.job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train_spa_gan \
+  --dataset horse2zebra \
+  --generator resnet \
+  --counterfactual_loss_weight 1 \
+  --attention clf
+
+
