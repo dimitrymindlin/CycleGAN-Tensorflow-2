5925dd49c9cef8c2715ec98951b827f4b1213777
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-20 16:31:27 +0200

    Changed slurm to start at checkpoint.

--- slurm/horse2zebra_attention.job.sh
+++ slurm/horse2zebra_attention.job.sh
@@ -4,9 +4,8 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train_spa_gan \
-  --dataset horse2zebra \
-  --attention_type spa-gan \
   --feature_map_loss_weight 1 \
+  --load_checkpoint 2022-05-17--17.39
 
 
 
