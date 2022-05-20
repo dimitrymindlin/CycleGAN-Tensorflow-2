92d1ff2d4d4d361e2f8a107c636e7dc96ff98d08
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-20 16:24:36 +0200

    Added new slurm cripts.

---
+++ slurm/horse2zebra_attention_gan.job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train_attention_gan
+
+
