4805375a6d78aa78205f949be7cc6eef530db2b2
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-23 22:34:40 +0200

    Prepared slurm to train normally.

--- slurm/horse2zebra.job.sh
+++ slurm/horse2zebra.job.sh
@@ -5,9 +5,5 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train \
   --dataset horse2zebra \
-  --load_checkpoint 2022-05-16--21.40 \
-  --feature_map_loss_weight 0 \
-  --attention_type none \
-  --generator resnet
 
 
