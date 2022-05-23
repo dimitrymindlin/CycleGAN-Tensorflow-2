a3892f02394ded3426b48be4646f46068fb38e10
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-23 23:31:11 +0200

    added slurms.

---
+++ slurm/train_clf.job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train_clf
+
