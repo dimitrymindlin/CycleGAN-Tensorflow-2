6353e91cafaf6f396813a33f53dc9d244853ed23
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-24 17:15:51 +0200

    Updated slurm.

--- slurm/horse2zebra_spa_attention.job.sh
+++ slurm/horse2zebra_spa_attention.job.sh
@@ -5,8 +5,7 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train_spa_gan \
   --dataset horse2zebra \
-  --generator resnet \
-  --discriminator_loss_weight 5 \
-  --feature_map_loss_weight 0
+  --generator resnet-attention \
+  --attention discriminator
 
 
