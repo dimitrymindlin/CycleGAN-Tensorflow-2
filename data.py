49fb6ea592dac355c5f95e23509a91d4bcc876f3
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-24 09:19:29 +0200

    Revert "everything to train clf."
    This reverts commit bcc92227ff03fe7e25988b0b9661b262f3fb4097.

--- data.py
+++ data.py
@@ -1,38 +1,24 @@
 import numpy as np
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
 import tf2lib as tl
-import tensorflow_addons as tfa
-import pylib as py
 
-def make_dataset(img_paths, batch_size, load_size, crop_size, training, drop_remainder=True, shuffle=True, repeat=1,
-                 labels=None, special_normalisation=None):
+
+def make_dataset(img_paths, batch_size, load_size, crop_size, training, drop_remainder=True, shuffle=True, repeat=1):
     if training:
-        #@tf.function
-        def _map_fn(img, label=None):  # preprocessing
-            #img = tfa.image.equalize(img)
+        @tf.function
+        def _map_fn(img):  # preprocessing
             img = tf.image.random_flip_left_right(img)
-            img = tf.image.resize_with_pad(img, load_size, load_size)
+            img = tf.image.resize(img, [load_size, load_size])
             img = tf.image.random_crop(img, [crop_size, crop_size, tf.shape(img)[-1]])
-            if not special_normalisation:
-                img = img / 255.0 * 2 - 1
-            else:
-                img = special_normalisation(img / 255.0)
-            if label is not None:
-                return img, label
+            img = tf.clip_by_value(img, 0, 255) / 255.0  # or img = tl.minmax_norm(img)
+            img = img * 2 - 1
             return img
     else:
         @tf.function
-        def _map_fn(img, label=None):  # preprocessing
-            #img = tfa.image.equalize(img)
-            img = tf.image.resize_with_pad(img, crop_size, crop_size)
-            if not special_normalisation:
-                img = img / 255.0 * 2 - 1
-            else:
-                img = special_normalisation(img / 255.0)
-            if label is not None:
-                return img, label
+        def _map_fn(img):  # preprocessing
+            img = tf.image.resize(img, [crop_size, crop_size])  # or img = tf.image.resize(img, [load_size, load_size]); img = tl.center_crop(img, crop_size)
+            img = tf.clip_by_value(img, 0, 255) / 255.0  # or img = tl.minmax_norm(img)
+            img = img * 2 - 1
             return img
 
     return tl.disk_image_batch_dataset(img_paths,
@@ -40,8 +26,7 @@ def make_dataset(img_paths, batch_size, load_size, crop_size, training, drop_rem
                                        drop_remainder=drop_remainder,
                                        map_fn=_map_fn,
                                        shuffle=shuffle,
-                                       repeat=repeat,
-                                       labels=labels)
+                                       repeat=repeat)
 
 
 def make_zip_dataset(A_img_paths, B_img_paths, batch_size, load_size, crop_size, training, shuffle=True, repeat=False):
@@ -56,27 +41,13 @@ def make_zip_dataset(A_img_paths, B_img_paths, batch_size, load_size, crop_size,
             A_repeat = None  # cycle the shorter one
             B_repeat = 1
 
-    A_dataset = make_dataset(A_img_paths, batch_size, load_size, crop_size, training, drop_remainder=True,
-                             shuffle=shuffle, repeat=A_repeat)
-    B_dataset = make_dataset(B_img_paths, batch_size, load_size, crop_size, training, drop_remainder=True,
-                             shuffle=shuffle, repeat=B_repeat)
+    A_dataset = make_dataset(A_img_paths, batch_size, load_size, crop_size, training, drop_remainder=True, shuffle=shuffle, repeat=A_repeat)
+    B_dataset = make_dataset(B_img_paths, batch_size, load_size, crop_size, training, drop_remainder=True, shuffle=shuffle, repeat=B_repeat)
 
     A_B_dataset = tf.data.Dataset.zip((A_dataset, B_dataset))
     len_dataset = max(len(A_img_paths), len(B_img_paths)) // batch_size
 
     return A_B_dataset, len_dataset
-
-
-def make_concat_dataset(A_img_paths, B_img_paths, batch_size, load_size, crop_size, training, shuffle=True,
-                        repeat=False, special_normalisation=None):
-    dataset_length = len(A_img_paths) + len(B_img_paths)
-    class_labels = [(1, 0) for _ in range(len(A_img_paths))]
-    class_labels.extend([(0, 1) for _ in range(len(B_img_paths))])
-    A_img_paths.extend(B_img_paths)  # becoming all_image_paths
-    all_image_paths = A_img_paths
-    return make_dataset(all_image_paths, batch_size, load_size, crop_size, training, drop_remainder=True,
-                        shuffle=shuffle, repeat=repeat, labels=class_labels,
-                        special_normalisation=special_normalisation), dataset_length
 
 
 class ItemPool:
@@ -104,61 +75,3 @@ class ItemPool:
                 else:
                     out_items.append(in_item)
         return tf.stack(out_items, axis=0)
-
-
-def get_mura_data_paths():
-    # To get the filenames for a task
-    def to_categorical(x, y):
-        y = [0 if x == 'negative' else 1 for x in y]
-        y = tf.keras.utils.to_categorical(y)
-        x, y = shuffle(x, y)
-        return x, y
-
-    def filenames(part, train=True):
-        root = '../tensorflow_datasets/downloads/cjinny_mura-v11/'
-        #root = '/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/'
-        if train:
-            csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
-            #csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/train_image_paths.csv"
-        else:
-            csv_path = "../tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
-            #csv_path = "/Users/dimitrymindlin/tensorflow_datasets/downloads/cjinny_mura-v11/MURA-v1.1/valid_image_paths.csv"
-
-        with open(csv_path, 'rb') as F:
-            d = F.readlines()
-            if part == 'all':
-                imgs = [root + str(x, encoding='utf-8').strip() for x in d]
-            else:
-                imgs = [root + str(x, encoding='utf-8').strip() for x in d if
-                        str(x, encoding='utf-8').strip().split('/')[2] == part]
-
-        # imgs= [x.replace("/", "\\") for x in imgs]
-        labels = [x.split('_')[-1].split('/')[0] for x in imgs]
-        return imgs, labels
-
-    part = 'XR_WRIST'  # part to work with
-    train_x, train_y = filenames(part=part)  # train data
-    test_x, test_y = filenames(part=part, train=False)  # test data
-    train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2,
-                                                          random_state=42)  # split train and valid data
-
-    train_x, train_y = to_categorical(train_x, train_y)
-    valid_x, valid_y = to_categorical(valid_x, valid_y)
-    test_x, test_y = to_categorical(test_x, test_y)
-
-    return train_x, train_y, valid_x, valid_y, test_x, test_y
-
-def get_dataset_paths(args):
-    if args.dataset == "mura":
-        # A = 0 = negative, B = 1 = positive
-        train_x, train_y, valid_x, valid_y, test_x, test_y = get_mura_data_paths()
-        A_img_paths = [filename for filename in train_x if "negative" in filename]
-        B_img_paths = [filename for filename in train_x if "positive" in filename]
-        A_img_paths_test = [filename for filename in train_x if "negative" in filename]
-        B_img_paths_test = [filename for filename in train_x if "positive" in filename]
-    else:
-        A_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainA'), '*.jpg')
-        B_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainB'), '*.jpg')
-        A_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testA'), '*.jpg')
-        B_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testB'), '*.jpg')
-    return A_img_paths, B_img_paths, A_img_paths_test, B_img_paths_test
