7d47ecfd3e7ecae1a5efdeb2df2a47391f0426b9
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-24 18:27:12 +0200

    New settings.

--- slurm/norm_1_10_1_0.job.sh
+++ slurm/norm_1_10_1_0.job.sh
@@ -3,9 +3,9 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train_spa_gan \
+python3 -m train \
   --dataset horse2zebra \
-  --generator resnet-attention \
-  --attention discriminator \
-  --counterfactual_loss_weight 1
-
+  --discriminator_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0
--- slurm/norm_1_10_1_1.job.sh
+++ slurm/norm_1_10_1_1.job.sh
@@ -5,4 +5,8 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train \
   --dataset horse2zebra \
+  --discriminator_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0
 
