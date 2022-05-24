cb2c5482a61e87747f690422c5586adb53b70e84
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-24 10:41:57 +0200

    updated slurm.

--- slurm/horse2zebra_attention.job.sh
+++ slurm/horse2zebra_attention.job.sh
@@ -4,8 +4,8 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train_spa_gan \
-  --feature_map_loss_weight 1 \
-  --load_checkpoint 2022-05-17--17.39
+  --feature_map_loss_weight 0 \
+  --counterfactual_loss_weight 1 \
 
 
 
