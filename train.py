60f36255e3173d753982b9e7fa900ca64c94bf54
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-30 07:28:34 +0200

    changed img size for norm.

--- train.py
+++ train.py
@@ -17,8 +17,8 @@ import module
 # ==============================================================================
 py.arg('--dataset', default='horse2zebra')
 py.arg('--datasets_dir', default='datasets')
-py.arg('--load_size', type=int, default=532)  # load image to this size
-py.arg('--crop_size', type=int, default=512)  # then crop to this size
+py.arg('--load_size', type=int, default=286)  # load image to this size
+py.arg('--crop_size', type=int, default=256)  # then crop to this size
 py.arg('--batch_size', type=int, default=1)
 py.arg('--epochs', type=int, default=200)
 py.arg('--epoch_decay', type=int, default=100)  # epoch to start decaying learning rate
