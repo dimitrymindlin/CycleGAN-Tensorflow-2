96055772ca05e8c24d96923dae97568c949d3a6b
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-31 13:33:35 +0200

    relocated data pipeline.

--- train.py
+++ train.py
@@ -63,81 +63,11 @@ py.args_to_yaml(py.join(output_dir, 'settings.yml'), args)
 # =                                    data                                    =
 # ==============================================================================
 
-
 A2B_pool = data.ItemPool(args.pool_size)
 B2A_pool = data.ItemPool(args.pool_size)
-AUTOTUNE = tf.data.AUTOTUNE
-dataset, metadata = tfds.load('cycle_gan/horse2zebra',
-                              with_info=True, as_supervised=True)
 
-train_horses, train_zebras = dataset['trainA'], dataset['trainB']
-test_horses, test_zebras = dataset['testA'], dataset['testB']
-len_dataset = 1334
-BUFFER_SIZE = 1000
-BATCH_SIZE = 1
-IMG_WIDTH = args.crop_size
-IMG_HEIGHT = args.crop_size
-
-
-def random_crop(image):
-    cropped_image = tf.image.random_crop(
-        image, size=[IMG_HEIGHT, IMG_WIDTH, 3])
-
-    return cropped_image
-
-
-# normalizing the images to [-1, 1]
-def normalize(image):
-    image = tf.cast(image, tf.float32)
-    image = tf.image.resize(image, [IMG_HEIGHT, IMG_WIDTH],
-                            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    image = (image / 127.5) - 1
-    return image
-
-
-def random_jitter(image):
-    # resizing to 286 x 286 x 3
-    image = tf.image.resize(image, [IMG_HEIGHT+30, IMG_WIDTH+30],
-                            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-    # randomly cropping to 256 x 256 x 3
-    image = random_crop(image)
-
-    # random mirroring
-    image = tf.image.random_flip_left_right(image)
-
-    return image
-
-
-def preprocess_image_train(image, label):
-    image = random_jitter(image)
-    image = normalize(image)
-    return image
-
-
-def preprocess_image_test(image, label):
-    image = normalize(image)
-    return image
-
-
-train_horses = train_horses.cache().map(
-    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
-    BUFFER_SIZE).batch(BATCH_SIZE)
-
-train_zebras = train_zebras.cache().map(
-    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
-    BUFFER_SIZE).batch(BATCH_SIZE)
-
-test_horses = test_horses.map(
-    preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
-    BUFFER_SIZE).batch(BATCH_SIZE)
-
-test_zebras = test_zebras.map(
-    preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
-    BUFFER_SIZE).batch(BATCH_SIZE)
-
-OUTPUT_CHANNELS = 3
-
+train_horses, train_zebras, test_horses, test_zebras, len_dataset = data.load_tfds_dataset(args.dataset,
+                                                                                           args.crop_size)
 # ==============================================================================
 # =                                   models                                   =
 # ==============================================================================
