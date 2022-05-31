79ce3706107822574e65843057563779e15f24fd
dimi9595 <dmitry.mindlin@gmail.com>
2022-05-31 13:00:41 +0200

    Added attention slurms.

---
+++ slurm/attention_1_10_1_0_0.job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train_attention_gan \
+  --dataset horse2zebra \
+  --discriminator_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0 \
+  --attention clf \
+  --generator resnet
+
