169c00cc241a25517a58dc18fbb2f3d9f23b6943
dimi9595 <dmitry.mindlin@gmail.com>
2022-06-02 12:48:55 +0200

    added new slurm.

---
+++ slurm/spa_5_5_1_0_0.job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train_spa_gan \
+  --dataset horse2zebra \
+  --discriminator_loss_weight 5 \
+  --cycle_loss_weight 5 \
+  --counterfactual_loss_weight 1 \
+  --feature_map_loss_weight 0 \
+  --identity_loss_weight 0 \
+  --attention clf \
+  --generator resnet \
+
