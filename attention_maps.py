19cbb97e7b855941ff45d56e968b90bb3faa7ba2
dimi9595 <dmitry.mindlin@gmail.com>
2022-06-05 10:38:40 +0200

    correcting stuff

--- attention_maps.py
+++ attention_maps.py
@@ -38,6 +38,6 @@ def apply_gradcam(img, gradcam, class_index, attention_type, attention_intensity
         cam = tf.ones(shape=cam.shape)
 
     # Interpolate by multiplication and normalise
-    img = tf.multiply(cam, img)
+    img = tf.math.multiply(cam, img)
     img = tf.math.divide(img, tf.reduce_max(img))
     return scale_to_minus_one_one(img), scale_to_minus_one_one(cam)  # [-1,1]
