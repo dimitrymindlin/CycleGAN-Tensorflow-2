0209f684b8b431dd748b72b1ab2e1659ba939a26
dimi9595 <dmitry.mindlin@gmail.com>
2022-06-06 22:02:32 +0200

    Added new slurms for mura.

---
+++ slurm/attention_1_10_1_0_mura.job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train_attention_gan_mura \
+  --dataset horse2zebra \
+  --discriminator_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --generator resnet
+
