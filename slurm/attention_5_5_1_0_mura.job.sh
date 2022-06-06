c0df1cdd848fe58c6034239b0829ac2897a08e9c
dimi9595 <dmitry.mindlin@gmail.com>
2022-06-06 23:53:36 +0200

    changed datasets.

--- slurm/attention_5_5_1_0_mura.job.sh
+++ slurm/attention_5_5_1_0_mura.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train_attention_gan_mura \
-  --dataset horse2zebra \
+  --dataset mura \
   --discriminator_loss_weight 5 \
   --cycle_loss_weight 5 \
   --counterfactual_loss_weight 1 \
