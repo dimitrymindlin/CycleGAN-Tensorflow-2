53ff65b6d3b764fbc253af29c837d2c1c888b232
dimi9595 <dmitry.mindlin@gmail.com>
2022-07-27 11:55:15 +0200

    added calc_G_loss function.

--- train.py
+++ train.py
@@ -103,47 +103,54 @@ if args.counterfactual_loss_weight > 0:
 # ==============================================================================
 # =                                 train step                                 =
 # ==============================================================================
+def calc_G_loss(A2B, B2A, A2B2A, B2A2B, A2A, B2B):
+    # Calculate Losses
+    A2B_d_logits = D_B(A2B, training=True)
+    B2A_d_logits = D_A(B2A, training=True)
+
+    if args.counterfactual_loss_weight > 0:
+        A2B_counterfactual_loss = counterfactual_loss_fn(class_B_ground_truth,
+                                                         clf(tf.image.resize(A2B, [512, 512])))
+        B2A_counterfactual_loss = counterfactual_loss_fn(class_A_ground_truth,
+                                                         clf(tf.image.resize(B2A, [512, 512])))
+    else:
+        A2B_counterfactual_loss = tf.zeros(())
+        B2A_counterfactual_loss = tf.zeros(())
+
+    A2B_g_loss = g_loss_fn(A2B_d_logits)
+    B2A_g_loss = g_loss_fn(B2A_d_logits)
+    A2B2A_cycle_loss = cycle_loss_fn(A, A2B2A)
+    B2A2B_cycle_loss = cycle_loss_fn(B, B2A2B)
+    A2A_id_loss = identity_loss_fn(A, A2A)
+    B2B_id_loss = identity_loss_fn(B, B2B)
+
+    G_loss = (A2B_g_loss + B2A_g_loss) + \
+             (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight \
+             + (A2A_id_loss + B2B_id_loss) * args.identity_loss_weight + \
+             (A2B_counterfactual_loss + B2A_counterfactual_loss) * args.counterfactual_loss_weight
+
+    G_loss_dict = {'A2B_g_loss': A2B_g_loss,
+                   'B2A_g_loss': B2A_g_loss,
+                   'A2B2A_cycle_loss': A2B2A_cycle_loss,
+                   'B2A2B_cycle_loss': B2A2B_cycle_loss,
+                   'A2A_id_loss': A2A_id_loss,
+                   'B2B_id_loss': B2B_id_loss,
+                   'A2B_counterfactual_loss': A2B_counterfactual_loss,
+                   'B2A_counterfactual_loss': B2A_counterfactual_loss}
+
+    return G_loss, G_loss_dict
 
 @tf.function
 def train_G(A, B):
     with tf.GradientTape() as t:
         A2B, B2A, A2B2A, B2A2B, A2A, B2B = attention_strategies.no_attention(A, B, G_A2B, G_B2A)
 
-        A2B_d_logits = D_B(A2B, training=True)
-        B2A_d_logits = D_A(B2A, training=True)
-
-        if args.counterfactual_loss_weight > 0:
-            A2B_counterfactual_loss = counterfactual_loss_fn(class_B_ground_truth,
-                                                             clf(tf.image.resize(A2B, [512, 512])))
-            B2A_counterfactual_loss = counterfactual_loss_fn(class_A_ground_truth,
-                                                             clf(tf.image.resize(B2A, [512, 512])))
-        else:
-            A2B_counterfactual_loss = 0
-            B2A_counterfactual_loss = 0
-
-        A2B_g_loss = g_loss_fn(A2B_d_logits)
-        B2A_g_loss = g_loss_fn(B2A_d_logits)
-        A2B2A_cycle_loss = cycle_loss_fn(A, A2B2A)
-        B2A2B_cycle_loss = cycle_loss_fn(B, B2A2B)
-        A2A_id_loss = identity_loss_fn(A, A2A)
-        B2B_id_loss = identity_loss_fn(B, B2B)
-
-        G_loss = (A2B_g_loss + B2A_g_loss) + \
-                 (A2B2A_cycle_loss + B2A2B_cycle_loss) * args.cycle_loss_weight + \
-                 (A2A_id_loss + B2B_id_loss) * args.identity_loss_weight + \
-                 (A2B_counterfactual_loss + B2A_counterfactual_loss) * args.counterfactual_loss_weight
+        G_loss, G_loss_dict = calc_G_loss(A2B, B2A, A2B2A, B2A2B, A2A, B2B)
 
     G_grad = t.gradient(G_loss, G_A2B.trainable_variables + G_B2A.trainable_variables)
     G_optimizer.apply_gradients(zip(G_grad, G_A2B.trainable_variables + G_B2A.trainable_variables))
 
-    return A2B, B2A, {'A2B_g_loss': A2B_g_loss,
-                      'B2A_g_loss': B2A_g_loss,
-                      'A2B2A_cycle_loss': A2B2A_cycle_loss,
-                      'B2A2B_cycle_loss': B2A2B_cycle_loss,
-                      'A2A_id_loss': A2A_id_loss,
-                      'B2B_id_loss': B2B_id_loss,
-                      'A2B_counterfactual_loss': A2B_counterfactual_loss,
-                      'B2A_counterfactual_loss': B2A_counterfactual_loss}
+    return A2B, B2A, G_loss_dict
 
 
 @tf.function
