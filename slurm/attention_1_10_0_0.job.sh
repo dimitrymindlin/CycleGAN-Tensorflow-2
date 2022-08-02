8e6b17670c9f4b1d047692c8658513884a6e0cda
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-02 09:52:16 +0200

    Added train_attention_gan2.py to find bugs.

--- slurm/attention_1_10_0_0.job.sh
+++ slurm/attention_1_10_0_0.job.sh
@@ -3,7 +3,7 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train_attention_gan \
+python3 -m train_attention_gan2 \
   --dataset horse2zebra \
   --discriminator_loss_weight 1 \
   --cycle_loss_weight 10 \
