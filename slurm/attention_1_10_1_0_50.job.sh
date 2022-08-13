3ef66aaebcbbf3fb9a0457266aaf4ac36c06af41
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-13 13:31:45 +0200

    Corrected slurms.

--- slurm/attention_1_10_1_0_50.job.sh
+++ slurm/attention_1_10_1_0_50.job.sh
@@ -3,12 +3,12 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train_attention_gan2 \
+python3 -m train_attention_gan \
   --dataset horse2zebra \
   --discriminator_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
   --identity_loss_weight 0 \
   --generator resnet \
-  --start_attention_epoch 100 \
+  --start_attention_epoch 50 \
 
