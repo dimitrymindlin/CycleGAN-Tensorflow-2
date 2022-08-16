432906ec0f426ddd6e09820489a764f809e5aeeb
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-16 10:40:13 +0200

    Added new slurms.

---
+++ slurm/norm_1_10_0_5.job.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train \
+  --dataset horse2zebra \
+  --discriminator_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 0 \
+  --identity_loss_weight 5
