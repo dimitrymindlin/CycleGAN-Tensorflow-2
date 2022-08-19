5ea173f4f119178646e4bf26d008f3ef7008972f
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-19 08:26:30 +0200

    new slurms.

---
+++ slurm/attention_5_5_1_0_5_mura.job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset mura \
+  --discriminator_loss_weight 5 \
+  --cycle_loss_weight 5 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0 \
+  --generator resnet \
+  --clf_ckp_name 2022-06-04--00.05 \
+  --start_attention_epoch 5 \
+
