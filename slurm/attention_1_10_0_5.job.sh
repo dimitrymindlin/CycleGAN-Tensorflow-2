a86926e8fa9868ff7302906519061faeeff3d5dd
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-22 09:41:13 +0200

    changed ds to apple2orange.

--- slurm/attention_1_10_0_5.job.sh
+++ slurm/attention_1_10_0_5.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m train_attention_gan \
-  --dataset horse2zebra \
+  --dataset apple2orange \
   --discriminator_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0 \
