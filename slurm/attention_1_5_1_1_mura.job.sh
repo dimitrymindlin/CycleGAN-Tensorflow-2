8823d34721d89e3b7d86719b274ea53da9a09f35
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-22 13:54:13 +0200

    changed slurm to use experiment.py

--- slurm/attention_1_5_1_1_mura.job.sh
+++ slurm/attention_1_5_1_1_mura.job.sh
@@ -3,7 +3,7 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train_attention_gan_mura \
+python3 -m experiment \
   --dataset mura \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 5 \
