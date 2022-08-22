32a757b8e4232dd2753276e67a2994aeb5bbd205
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-22 21:49:38 +0200

    Adapted script to support attention gan and no_attention strategies.

--- evaluation/tcv_os.py
+++ evaluation/tcv_os.py
@@ -1,53 +1,28 @@
 import numpy as np
 import tqdm
 
-from imlib.image_holder import ImageHolder, multiply_images, add_images
+from attention_strategies.attention_gan import attention_gan_single
+from attention_strategies.no_attention import no_attention_single
+from imlib.image_holder import ImageHolder
 import tensorflow as tf
 
 
-@tf.function
-def sample(G, img):
-    """
-
-    Parameters
-    ----------
-    G: Generator
-    img: Image
-
-    Returns translated image
-    -------
-
-    """
-    return G(img, training=False)
-
-
-def calculate_tcv_os(clf, oracle, G_A2B, G_B2A, dataset, translation_name, gradcam, return_images=False):
-    len_dataset = 0
+def translate_images_clf_oracle(dataset, clf, oracle, generator, gradcam, class_label, return_images, attention_type,
+                                training=False):
     translated_images = []
     y_pred_translated = []
     y_pred_oracle = []
+    len_dataset = 0
+
     for img_batch in tqdm.tqdm(dataset):
-        if translation_name == "A2B":
-            generator = G_A2B
-            cycle_ganarator = G_B2A
-            img_holder = ImageHolder(img_batch, 0, gradcam=gradcam, attention_type="attention-gan")
+        img_holder = ImageHolder(img_batch, class_label, gradcam=gradcam, attention_type=attention_type)
+        if attention_type == "attention-gan":
+            translated_img, _ = attention_gan_single(img_holder.img, generator, None, img_holder.attention,
+                                                     img_holder.background, training)
         else:
-            generator = G_B2A
-            cycle_ganarator = G_A2B
-            img_holder = ImageHolder(img_batch, 1, gradcam=gradcam, attention_type="attention-gan")
-
-        img_transformed = sample(generator, img_holder.img)
-        # Combine new transformed image with attention -> Crop important part from transformed img
-        img_transformed_attention = multiply_images(img_transformed, img_holder.attention)
-        # Add background to new img
-        translated_img = add_images(img_transformed_attention, img_holder.background)
-        # Cycle
-        img_cycled = sample(cycle_ganarator, translated_img)
-        # Combine new transformed image with attention
-        img_cycled_attention = multiply_images(img_cycled, img_holder.attention)
-        cycled_img = add_images(img_cycled_attention, img_holder.background)
-
-        for img_i, translated_i, cycled_i in zip(img_batch, translated_img, cycled_img):
+            translated_img = no_attention_single(img_holder.img, generator, None, training)
+        #
+        for img_i, translated_i in zip(img_batch, translated_img):
             if return_images:
                 translated_images.append(tf.squeeze(translated_i))
             y_pred_translated.append(
@@ -56,7 +31,25 @@ def calculate_tcv_os(clf, oracle, G_A2B, G_B2A, dataset, translation_name, gradc
                 int(np.argmax(oracle(tf.expand_dims(translated_i, axis=0)))))
 
             len_dataset += 1
+    return y_pred_translated, y_pred_oracle, len_dataset, translated_images
 
+
+def calculate_tcv_os(clf, oracle, G_A2B, G_B2A, dataset, translation_name, gradcam, attention_type,
+                     return_images=False):
+    if translation_name == "A2B":
+        generator = G_A2B
+        class_label = 0
+    else:
+        generator = G_B2A
+        class_label = 1
+
+    # Get translated images
+    y_pred_translated, y_pred_oracle, len_dataset, translated_images = translate_images_clf_oracle(dataset, clf, oracle,
+                                                                                                   generator,
+                                                                                                   gradcam, class_label,
+                                                                                                   attention_type,
+                                                                                                   return_images)
+    # Calculate tcv and os
     if translation_name == "A2B":
         tcv = sum(y_pred_translated) / len_dataset
         similar_predictions_count = sum(x == y == 1 for x, y in zip(y_pred_translated, y_pred_oracle))
