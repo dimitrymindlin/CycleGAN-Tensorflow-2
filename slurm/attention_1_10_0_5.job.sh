08e904b6b8a9d8cad1e0203e9070d2523a0f3f5a
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-22 10:33:45 +0200

    fixed script name.

--- slurm/attention_1_10_0_5.job.sh
+++ slurm/attention_1_10_0_5.job.sh
@@ -3,7 +3,7 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train_attention_gan \
+python3 -m experiment \
   --dataset apple2orange \
   --discriminator_loss_weight 1 \
   --cycle_loss_weight 10 \
