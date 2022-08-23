a3aae18629990e4daf7d7e231c17fb57df472c80
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-23 22:39:25 +0200

    Added slurms for testing.

---
+++ slurm/test_counterfactuals_abc.job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2/test_scripts
+
+python3 -m test_counterfactuals --dataset mura --counterfactuals abc-gan
+
