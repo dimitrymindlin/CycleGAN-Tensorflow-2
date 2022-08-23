dda77a0ef0d3cea9fe6f9ad4328bce5c93c54128
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-23 23:09:09 +0200

    adapting paths.

--- slurm/test_counterfactuals_ganterfactual.job.sh
+++ slurm/test_counterfactuals_ganterfactual.job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
-cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2/
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m test_counterfactuals --dataset mura --counterfactuals ganterfactual
 
