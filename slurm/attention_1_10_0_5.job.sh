e1d2cb7c52163b41e537abb1b296cb56762bd420
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-23 23:10:35 +0200

    Added counterfactual loss.

--- slurm/attention_1_10_0_5.job.sh
+++ slurm/attention_1_10_0_5.job.sh
@@ -7,7 +7,7 @@ python3 -m experiment \
   --dataset horse2zebra \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
-  --counterfactual_loss_weight 0 \
+  --counterfactual_loss_weight 1 \
   --identity_loss_weight 5 \
   --generator resnet \
   --start_attention_epoch 50 \
