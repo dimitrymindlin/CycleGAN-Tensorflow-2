8e56d0121d20b2145c0a8cdc66cfb400d65b99ac
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-24 11:59:05 +0200

    One less epoch for mura.

--- experiment.py
+++ experiment.py
@@ -37,7 +37,7 @@ args = py.args()
 if args.dataset == "mura":
     args.load_size = 512
     args.crop_size = 512
-    args.epochs = 18
+    args.epochs = 17
     args.epoch_decay = 15
     args.sample_interval = 2
 
