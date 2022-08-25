964085b6bd246011c0997d6e1510858a6a67040e
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-26 00:04:35 +0200

    CPU path.

--- test_counterfactuals.py
+++ test_counterfactuals.py
@@ -39,7 +39,7 @@ if len(tf.config.list_physical_devices('GPU')) == 0:
 else:
     TFDS_PATH = "../tensorflow_datasets"
 
-#TFDS_PATH = "../tensorflow_datasets"
+TFDS_PATH = "../tensorflow_datasets"
 
 if args.dataset == "mura":
     args.crop_size = 512
