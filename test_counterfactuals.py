703843b77899893318f2a7552412a88bb61d9e08
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-28 17:48:24 +0100

    Added new model.

--- test_counterfactuals.py
+++ test_counterfactuals.py
@@ -1,7 +1,6 @@
 import os
 import sys
 
-import numpy as np
 from mura.tfds_from_disc import get_mura_test_ds_by_body_part_split_class
 from tf_keras_vis.gradcam_plus_plus import GradcamPlusPlus
 import pylib as py
@@ -10,7 +9,7 @@ import tf2lib as tl
 import module
 from evaluation.kid import calc_KID_for_model_target_source, calc_KID_for_model
 from evaluation.load_test_data import load_h2z_test_data
-from evaluation.tcv_os import calculate_tcv_os, translate_images_clf_oracle
+from evaluation.tcv_os import calculate_tcv_os, translate_images_clf_oracle, calculate_ssim_psnr
 from tensorflow_addons.layers import InstanceNormalization
 
 # ==============================================================================
@@ -18,7 +17,7 @@ from tensorflow_addons.layers import InstanceNormalization
 # ==============================================================================
 from imlib import save_mura_images, immerge, imwrite
 
-py.arg('--dataset', default='mura', choices=['horse2zebra', 'mura', 'apple2orange'])
+py.arg('--dataset', default='horse2zebra', choices=['horse2zebra', 'mura', 'apple2orange'])
 py.arg('--body_parts', default=["XR_WRIST"])  # Only used in Mura dataset. Body part of x-ray images
 py.arg('--batch_size', type=int, default=1)
 py.arg('--datasets_dir', default='datasets')
@@ -32,6 +31,9 @@ py.arg('--print_images', type=bool, default=True)
 py.arg('--crop_size', type=int, default=256)  # Mura: 512 H2Z: 256
 py.arg('--gan_model_ts', type=str, default="2022-05-26--15.51")
 py.arg('--counterfactuals', type=str, default="abc-gan", choices=["abc-gan", "ganterfactual"])
+py.arg('--tcv_os', type=bool, default=False)
+py.arg('--ssim_psnr', type=bool, default=True)
+py.arg('--kid', type=bool, default=False)
 args = py.args()
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 if len(tf.config.list_physical_devices('GPU')) == 0:
@@ -39,7 +41,7 @@ if len(tf.config.list_physical_devices('GPU')) == 0:
 else:
     TFDS_PATH = "../tensorflow_datasets"
 
-TFDS_PATH = "../tensorflow_datasets"
+# TFDS_PATH = "../tensorflow_datasets"
 
 if args.dataset == "mura":
     args.crop_size = 512
@@ -108,6 +110,9 @@ checkpoint_ts_list_mura = ["2022-08-18--17.48", "2022-08-19--08.32", "2022-08-19
                            "2022-08-22--14.00"]  # "2022-08-18--17.48",
 checkpoint_ep_list_mura = ["20", "20", "24", "14", "16"]  # "20",
 
+checkpoint_ts_list_mura = ["2022-08-27--18.00", "2022-08-27--18.00"]
+checkpoint_ep_list_mura = ["14", "16"]
+
 checkpoint_ts_list_h2z = ["2022-08-13--15.48"]  # "2022-08-17--03.54"
 checkpoint_ep_list_h2z = ["195"]  # "180"
 
@@ -142,17 +147,24 @@ def evaluate_current_model(G_A2B, G_B2A, save_img=False):
             source_dataset = B_dataset_test
             target_dataset = A_dataset
 
+        # Get counterfactuals (translated images)
         y_pred_translated, y_pred_oracle, len_dataset, translated_images = translate_images_clf_oracle(
             source_dataset, clf, oracle, generator, gradcam, class_label, True, args.attention_type,
             training=False, save_img=save_img)
-        tcv, os = calculate_tcv_os(y_pred_translated, y_pred_oracle, len_dataset, translation_name)
-        print(tcv)
-        print(os)
-        if args.dataset == "mura":
-            calc_KID_for_model(translated_images, args.crop_size, target_dataset)
-        else:
-            calc_KID_for_model_target_source(translated_images, translation_name, args.crop_size, A_dataset,
-                                             B_dataset)
+
+        if args.tcv_os:
+            calculate_tcv_os(y_pred_translated, y_pred_oracle, len_dataset, translation_name)
+
+        if args.ssim_psnr:
+            calculate_ssim_psnr(source_dataset, translated_images)
+
+        if args.kid:
+            if args.dataset == "mura":
+                calc_KID_for_model(translated_images, args.crop_size, target_dataset)
+            else:
+                calc_KID_for_model_target_source(translated_images, translation_name, args.crop_size, A_dataset,
+                                                 B_dataset)
+    print()
 
 
 with open(f'{args.counterfactuals}_{args.dataset}.txt', 'w') as f:
