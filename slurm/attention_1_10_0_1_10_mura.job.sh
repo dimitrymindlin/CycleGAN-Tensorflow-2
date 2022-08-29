fc7a82be9e0deae117c78a8c0cd745ad26771ee3
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-29 23:14:07 +0200

    changed slurm.

--- slurm/attention_1_10_0_1_10_mura.job.sh
+++ slurm/attention_1_10_0_1_10_mura.job.sh
@@ -6,7 +6,7 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 python3 -m experiment \
   --dataset mura \
   --adversarial_loss_weight 1 \
-  --cycle_loss_weight 5 \
+  --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0 \
   --identity_loss_weight 1 \
   --clf_ckp_name 2022-06-04--00.05 \
