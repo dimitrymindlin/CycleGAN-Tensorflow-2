b184597487b33255bd96dfdd6d0851ac36df6e2a
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-29 23:19:32 +0200

    changed slurm again

--- slurm/attention_1_10_0_5_10_mura.job.sh
+++ slurm/attention_1_10_0_5_10_mura.job.sh
@@ -8,7 +8,7 @@ python3 -m experiment \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 0 \
-  --identity_loss_weight 1 \
+  --identity_loss_weight 5 \
   --clf_ckp_name 2022-06-04--00.05 \
   --generator resnet \
   --start_attention_epoch 10 \
