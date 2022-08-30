eb263623b3f261fb1f8682052b5dc3ace0893ac0
dimi9595 <dmitry.mindlin@gmail.com>
2022-08-30 14:15:57 +0200

    Added slum for cyclegan evaluation.

---
+++ slurm/test_counterfactuals_normal_cpu.job.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m test_counterfactuals --dataset mura --counterfactuals none --save_img True --tcv_os True --ssim_psnr True --kid True
+
