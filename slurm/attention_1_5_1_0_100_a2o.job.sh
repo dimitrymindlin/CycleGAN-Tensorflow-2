d25e16a4d6f2575fe8d8298c8bcdd40393870bb7
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-23 16:19:44 +0200

    corrected dataset.

--- slurm/attention_1_5_1_0_100_a2o.job.sh
+++ slurm/attention_1_5_1_0_100_a2o.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset mura \
+  --dataset apple2orange \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 5 \
   --counterfactual_loss_weight 1 \
