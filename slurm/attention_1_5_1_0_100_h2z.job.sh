5b7de327d200bfc6256d443ade3bba7e1ec3af1f
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-23 16:19:54 +0200

    new slurms for h2z ABC-GAN.

---
+++ slurm/attention_1_5_1_0_100_h2z.job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset horse2zebra \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 5 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0 \
+  --generator resnet \
+  --clf_ckp_name 2022-06-04--00.00 \
+  --discriminator patch_gan \
+  --epoch_decay 150 \
+  --start_attention_epoch 100 \
+
