1c281dba02fcd8bac59902b31dfe03304a54e649
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-26 13:40:04 +0200

    added clf_ckp_name

--- slurm/test_counterfactuals_abc.job.sh
+++ slurm/test_counterfactuals_abc.job.sh
@@ -3,5 +3,13 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m test_counterfactuals --dataset horse2zebra --counterfactuals_type abc-gan --tcv_os True --ssim_psnr True --kid True --save_img True
+python3 -m test_counterfactuals \
+--dataset horse2zebra \
+--counterfactuals_type abc-gan \
+--tcv_os True \
+--ssim_psnr True \
+--kid True \
+--save_img True \
+--clf_ckp_name 2022-08-21--00.00 \
+
 
