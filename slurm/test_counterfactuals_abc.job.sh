02e32551bce75e36c8a7e2257da511cdd581943e
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-26 13:00:27 +0200

    Adapted slurm.

--- slurm/test_counterfactuals_abc.job.sh
+++ slurm/test_counterfactuals_abc.job.sh
@@ -3,5 +3,5 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m test_counterfactuals --dataset mura --counterfactuals abc-gan --tcv_os False --ssim_psnr False --kid False --save_img True
+python3 -m test_counterfactuals --dataset horse2zebra --counterfactuals_type abc-gan --tcv_os True --ssim_psnr True --kid True --save_img True
 
