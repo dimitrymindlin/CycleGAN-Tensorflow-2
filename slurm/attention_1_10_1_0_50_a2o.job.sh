72b730c50bd5fadb5309cab79159229d360c5da2
dimi9595 <dmitry.mindlin@gmail.com>
2022-09-29 16:20:01 +0200

    new slurm weight settings.

--- slurm/attention_1_10_1_0_50_a2o.job.sh
+++ slurm/attention_1_10_1_0_50_a2o.job.sh
@@ -4,13 +4,13 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset horse2zebra \
+  --dataset apple2orange \
   --adversarial_loss_weight 1 \
-  --cycle_loss_weight 5 \
+  --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
   --identity_loss_weight 0 \
   --generator resnet \
-  --clf_ckp_name 2022-06-04--00.00 \
+  --clf_ckp_name 2022-09-23--15.18 \
   --discriminator patch-gan \
   --epoch_decay 100 \
   --start_attention_epoch 50 \
--- slurm/attention_1_10_1_0_50_h2z.job.sh
+++ slurm/attention_1_10_1_0_50_h2z.job.sh
@@ -4,13 +4,13 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset apple2orange \
+  --dataset horse2zebra \
   --adversarial_loss_weight 1 \
-  --cycle_loss_weight 5 \
+  --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
   --identity_loss_weight 0 \
   --generator resnet \
-  --clf_ckp_name 2022-09-23--15.18 \
+  --clf_ckp_name 2022-06-04--00.00 \
   --discriminator patch-gan \
   --epoch_decay 100 \
   --start_attention_epoch 50 \
