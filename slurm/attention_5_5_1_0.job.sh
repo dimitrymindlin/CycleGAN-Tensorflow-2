9edbfaa58e10785cbe383c616ec03c3ffd694443
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-12 14:57:05 +0200

    corrected script path.

--- slurm/attention_5_5_1_0.job.sh
+++ slurm/attention_5_5_1_0.job.sh
@@ -3,7 +3,7 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train_attention_gan \
+python3 -m experiment \
   --dataset rsna \
   --adversarial_loss_weight 5 \
   --cycle_loss_weight 5 \
