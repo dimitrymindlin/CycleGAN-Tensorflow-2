2ef721631e223817f89d7bdf01006a18f6286e2c
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-20 16:56:08 +0200

    Added slurm for h2z.

--- slurm/test_counterfactuals_ganterfactual.job.sh
+++ slurm/test_counterfactuals_ganterfactual.job.sh
@@ -3,5 +3,12 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m test_counterfactuals --dataset mura --counterfactuals ganterfactual --save_img True --tcv_os True --ssim_psnr True --kid True
+python3 -m test_counterfactuals \
+--dataset horse2zebra \
+--tcv_os True \
+--ssim_psnr True \
+--kid True \
+--save_img False \
+
+
 
