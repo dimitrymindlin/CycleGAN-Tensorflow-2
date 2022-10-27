f2754564d9b18a672ebc7ed83efd9a1821206f12
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-27 18:20:36 +0200

    Added slurm for ganterfactual a2o.

---
+++ slurm/ganterfactual_a2o.job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset apple2orange \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --generator resnet \
+  --epoch_decay 100 \
+  --start_attention_epoch 199 \
+  --discriminator patch-gan \
+
