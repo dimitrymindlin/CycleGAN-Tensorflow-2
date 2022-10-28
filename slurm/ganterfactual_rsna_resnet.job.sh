181cd5e89ada04760b4561d41372d45e73b9b8a6
dimi9595 <dmitry.mindlin@gmail.com>
2022-10-28 15:42:16 +0200

    Added new slurms.

---
+++ slurm/ganterfactual_rsna_resnet.job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset resnet \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --generator resnet \
+  --discriminator patch-gan \
+
