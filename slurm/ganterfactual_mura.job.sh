f28ecba101c7dbebaab4fa720b03432ab1d7be67
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-02 16:37:21 +0100

    Test with mura.

---
+++ slurm/ganterfactual_mura.job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset mura \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --generator resnet \
+  --clf_name inception \
+  --attention_type none \
+
