94640386fd07198ccaa21498e17ad3264c9e7dbd
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-04 14:31:42 +0100

    Added new slurms.

---
+++ slurm/ganterfactual_1_5_1_1_mura.job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset mura \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 5 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --clf_ckp_name 2022-06-04--00.05 \
+  --generator resnet \
+  --clf_name inception \
+  --start_attention_epoch 0 \
+  --attention_type none
+
