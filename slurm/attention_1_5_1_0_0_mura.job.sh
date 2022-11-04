3cc3c5f1db8b6fb1df8ed61dc793a406a89de131
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-04 14:16:49 +0100

    New slurm

--- slurm/attention_1_5_1_0_0_mura.job.sh
+++ slurm/attention_1_5_1_0_0_mura.job.sh
@@ -6,10 +6,11 @@ cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 python3 -m experiment \
   --dataset mura \
   --adversarial_loss_weight 1 \
-  --cycle_loss_weight 10 \
-  --counterfactual_loss_weight 0 \
-  --identity_loss_weight 5 \
+  --cycle_loss_weight 5 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0 \
   --clf_ckp_name 2022-06-04--00.05 \
   --generator resnet \
-  --start_attention_epoch 10 \
+  --clf_name inception \
+  --start_attention_epoch 0 \
 
