06672080fb58e966058d77bf23616e47a03803d6
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-06 18:13:55 +0100

    Added new slurm.

---
+++ slurm/test_counterfactuals_mura.job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m test_counterfactuals \
+--dataset mura \
+--tcv_os True \
+--ssim_psnr True \
+--kid True \
+--save_img False \
+
+
