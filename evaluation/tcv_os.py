91bc7d5bcc24353743041f80e0be25bd06682daa
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-07 13:50:37 +0100

    fixed argument name.

--- evaluation/tcv_os.py
+++ evaluation/tcv_os.py
@@ -110,7 +110,7 @@ def translate_images_clf(args, dataset, clf, generator, gradcam, class_label, re
                     original_img_batched = tf.image.rgb_to_grayscale(original_img_batched)
                 original_prediction = int(np.argmax(clf(original_img_batched)))
                 if args.dataset == "apple2orange":
-                    if args.attention != "none":
+                    if args.attention_type != "none":
                         img = immerge(np.concatenate([img_holder.img, img_holder.attention, translated_img], axis=0),
                                       n_rows=1)
                     else:
