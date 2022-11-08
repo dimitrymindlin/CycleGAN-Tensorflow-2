945454cc4e578f9b49f15b5c0e8cb66e82322aa1
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-08 13:35:29 +0100

    Removed method translate_images_clf_oracle.

--- evaluation/tcv_os.py
+++ evaluation/tcv_os.py
@@ -14,69 +14,6 @@ from imlib.image_holder import ImageHolder
 import tensorflow as tf
 
 
-def translate_images_clf_oracle(dataset, clf, oracle, generator, gradcam, class_label, return_images, attention_type,
-                                training=False, save_img=False):
-    translated_images = []
-    y_pred_translated = []
-    y_pred_oracle = []
-    len_dataset = 0
-
-    for batch_i, img_batch in enumerate(tqdm.tqdm(dataset, desc='Translating images')):
-        img_holder = ImageHolder(img_batch, class_label, gradcam=gradcam, attention_type=attention_type)
-        class_label_name = "Normal" if class_label == 0 else "Abnormal"
-        target_class_name = "Abnormal" if class_label == 0 else "Normal"
-        if attention_type == "attention-gan-original":
-            translated_img, _ = attention_gan_single(img_holder.img, generator, None, img_holder.attention,
-                                                     img_holder.background, training)
-        else:
-            translated_img = no_attention_single(img_holder.img, generator, None, training)
-        # Predict images with CLF and Oracle
-        for img_i, translated_i in zip(img_batch, translated_img):
-            if return_images:
-                translated_images.append(tf.squeeze(translated_i))
-            clf_prediction = int(np.argmax(clf(tf.expand_dims(tf.image.resize(translated_i, [512, 512]), axis=0))))
-            y_pred_translated.append(clf_prediction)
-            oracle_prediction = int(np.argmax(oracle(tf.expand_dims(translated_i, axis=0))))
-            y_pred_oracle.append(oracle_prediction)
-            if save_img:
-                """img = immerge(np.concatenate([img_holder.img, img_holder.attention, translated_img], axis=0), n_rows=1)
-                class_label_name = "Normal" if class_label == 0 else "Abnormal"
-                img_folder = f'output_mura/{class_label_name}'
-                py.mkdir(img_folder)
-                imwrite(img, f"{img_folder}/%d.png" % (batch_i))"""
-                r, c = 1, 3
-                titles = ['Original', 'Attention', 'Output']
-                imgs = [img_holder.img, img_holder.attention, translated_img]
-                original_prediction = int(np.argmax(clf(tf.expand_dims(tf.image.resize(img_i, [512, 512]), axis=0))))
-                classification = [original_prediction, "", clf_prediction]
-                gen_imgs = np.concatenate(imgs)
-                gen_imgs = 0.5 * gen_imgs + 0.5
-                correct_classification = [class_label_name, class_label_name, target_class_name]
-                fig, axs = plt.subplots(r, c, figsize=(30, 20))
-                cnt = 0
-
-                for j in range(c):
-                    axs[j].imshow(gen_imgs[cnt][:, :, 0], cmap='gray')
-                    if j == 2:
-                        axs[j].set_title(
-                            f'{titles[j]} (T: {correct_classification[cnt]} | P: {classification[cnt]}, O:{oracle_prediction})')
-                    elif j == 1:
-                        axs[j].set_title(
-                            f'{titles[j]}')
-                    else:
-                        axs[j].set_title(
-                            f'{titles[j]} (T: {correct_classification[cnt]} | P: {classification[cnt]}')
-                    axs[j].axis('off')
-                    cnt += 1
-                img_folder = f'{save_img}/{class_label_name}'
-                os.makedirs(img_folder, exist_ok=True)
-                fig.savefig(f"{img_folder}/%d.png" % (batch_i))
-                plt.close()
-        len_dataset += 1
-
-    return y_pred_translated, y_pred_oracle, len_dataset, translated_images
-
-
 def translate_images_clf(args, dataset, clf, generator, gradcam, class_label, return_images,
                          training=False, save_img=False):
     translated_images = []
