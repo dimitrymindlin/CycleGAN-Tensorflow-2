0c3af983340c63fce018c6db06ca74e45e014dd2
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-15 15:59:49 +0100

    Added normal cyclegan slurms.

---
+++ slurm/test_counterfactuals_a2o_normal.job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m test_counterfactuals \
+--dataset apple2orange \
+--tcv_os True \
+--ssim_psnr True \
+--kid True \
+--save_img False \
+--clf_name inception \
+--generator resnet \
+--attention_type none \
+--cyclegan_mode normal
+
