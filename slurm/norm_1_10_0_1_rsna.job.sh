3e1f1c36577925a4afed971251ffa7fe7b729490
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-18 11:04:05 +0100

    Created slurms for normal rsna run.

--- slurm/norm_1_10_0_1_rsna.job.sh
+++ slurm/norm_1_10_0_1_rsna.job.sh
@@ -3,9 +3,10 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m train \
-  --dataset horse2zebra \
+python3 -m experiment \
+  --dataset rsna \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
-  --counterfactual_loss_weight 1 \
-  --identity_loss_weight 0
+  --counterfactual_loss_weight 0 \
+  --identity_loss_weight 1 \
+  --attention_type none
