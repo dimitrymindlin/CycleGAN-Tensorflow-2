66b442da21de4d88250f2b74b31886946407ce19
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-18 14:19:05 +0100

    Fixed len() before list.

--- evaluation/kid.py
+++ evaluation/kid.py
@@ -113,7 +113,7 @@ def calc_KID_for_model_target_source(translated_images, translation_name, img_sh
 
 def calc_KID_for_model(translated_images, img_shape, dataset):
     # Standard KID calculation of translated images with target domain.
-    max_samples = 600
+    max_samples = 650
 
     # Check if one channel images and if so, turn to 3 channel images.
     if img_shape[-1] == 1:
@@ -142,7 +142,7 @@ def calc_KID_for_model(translated_images, img_shape, dataset):
                 else:
                     current_samples = tmp_samples[j * max_samples:(j + 1) * max_samples]
                     current_translated_images = translated_images[j * max_samples:(j + 1) * max_samples]
-                if current_translated_images < 100:
+                if len(current_translated_images) < 100:
                     break
 
                 tmp_samples_tensor = tf.convert_to_tensor(tf.squeeze(current_samples))
@@ -150,7 +150,7 @@ def calc_KID_for_model(translated_images, img_shape, dataset):
                 if len(tf.shape(tmp_samples_tensor)) < 4:  # channel dimension lost in squeeze because was 1 channel tensor.
                     tmp_samples_tensor = tf.image.grayscale_to_rgb(tf.expand_dims(tf.squeeze(tmp_samples_tensor), axis=-1))
 
-                kid.update_state(tmp_samples_tensor, translated_images)
+                kid.update_state(tmp_samples_tensor, current_translated_images)
         else:
             tmp_samples_tensor = tf.convert_to_tensor(tf.squeeze(tmp_samples))
             if len(tf.shape(tmp_samples_tensor)) < 4:  # channel dimension lost in squeeze because was 1 channel tensor.
