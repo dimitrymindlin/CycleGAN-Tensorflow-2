68e345cf0638fcab5dbcbed066bc3a59435d6567
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-22 13:29:53 +0100

    new slurm.

---
+++ slurm/attention_1_5_1_1_rsna.job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset rsna \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 5 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --generator resnet \
+  --clf_name alexnet
+
