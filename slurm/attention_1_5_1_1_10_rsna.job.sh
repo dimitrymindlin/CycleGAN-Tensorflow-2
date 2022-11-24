9584052b92758e651c6ff09a2771e70521c3bd0b
dimi9595 <dmitry.mindlin@gmail.com>
2022-11-24 09:06:16 +0100

    changed slurm.

--- slurm/attention_1_5_1_1_10_rsna.job.sh
+++ slurm/attention_1_5_1_1_10_rsna.job.sh
@@ -4,13 +4,11 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset mura \
+  --dataset rsna \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 5 \
   --counterfactual_loss_weight 1 \
   --identity_loss_weight 1 \
   --generator resnet \
-  --clf_ckp_name 2022-06-04--00.05 \
-  --discriminator patch_gan_attention \
   --start_attention_epoch 10 \
 
