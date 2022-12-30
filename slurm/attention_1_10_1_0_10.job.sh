53ffb10b6288193cd7e51762084d0baab517e55b
dimi9595 <dmitry.mindlin@gmail.com>
2022-12-30 13:58:42 +0100

    changed to use a2o.

--- slurm/attention_1_10_1_0_10.job.sh
+++ slurm/attention_1_10_1_0_10.job.sh
@@ -4,13 +4,13 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset mura \
+  --dataset apple2orange \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
   --identity_loss_weight 0 \
   --generator resnet \
-  --clf_name alexnet \
+  --clf_name inception \
   --start_attention_epoch 10 \
   --discriminator patch_gan_attention
 
