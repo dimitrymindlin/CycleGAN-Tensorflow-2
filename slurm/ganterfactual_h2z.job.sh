800e2718213a04422ba6f2f0da7d8b8724bf4fb0
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-04 09:49:13 +0100

    added gtf h2z job.

---
+++ slurm/ganterfactual_h2z.job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset horse2zebra \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 1 \
+  --generator resnet \
+  --epoch_decay 100 \
+  --start_attention_epoch 199 \
+  --discriminator patch-gan \
+  --attention_type none
+
