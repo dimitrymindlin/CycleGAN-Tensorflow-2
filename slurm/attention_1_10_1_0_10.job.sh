da2c3c8a4868de1705251458255e120e673e2fa2
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-04 15:17:32 +0100

    Changed to run for h2z.

--- slurm/attention_1_10_1_0_10.job.sh
+++ slurm/attention_1_10_1_0_10.job.sh
@@ -4,7 +4,7 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset apple2orange \
+  --dataset horse2zebra \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
