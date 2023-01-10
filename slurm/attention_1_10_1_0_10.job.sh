2be8fc1e00da808350b6785de48a20faaa874df8
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-10 05:26:43 +0100

    changed to use mura dataset.

--- slurm/attention_1_10_1_0_10.job.sh
+++ slurm/attention_1_10_1_0_10.job.sh
@@ -4,13 +4,13 @@ conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
 python3 -m experiment \
-  --dataset horse2zebra \
+  --dataset mura \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
   --identity_loss_weight 0 \
   --generator resnet \
-  --clf_name inception \
-  --start_attention_epoch 100 \
+  --clf_name alexnet \
+  --start_attention_epoch 10 \
   --discriminator patch_gan_attention
 
