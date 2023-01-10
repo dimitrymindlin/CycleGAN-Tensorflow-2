ae40f400b0c843182592b9719be0204a82558095
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-10 20:00:26 +0100

    Added new slurms.

---
+++ slurm/abc-gan_continue.job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m experiment \
+  --dataset rsna \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 1 \
+  --identity_loss_weight 0 \
+  --generator resnet \
+  --clf_name alexnet \
+  --start_attention_epoch 10 \
+  --discriminator patch_gan_attention \
+  --load_checkpoint "2023-01-05--09.51"
+
