8252143fa208c2f935963d2adfb573916e527292
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-11 19:31:35 +0100

    changed to train h2z on simpler clf.

--- train_clf.py
+++ train_clf.py
@@ -1,24 +1,33 @@
+import json
+import os
 from datetime import datetime
 
+import numpy as np
+from rsna.tfds_from_disc import get_rsna_ds_for_clf
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import tensorflow_addons as tfa
 import pylib as py
 import tensorflow as tf
 import tensorflow.keras as keras
 
 import standard_datasets_loading
-from classifiers.domain_to_domain_model import Domain2DomainModel
+from classifiers.domain_to_domain_model import Domain2DomainModel, CatsVSDogsModel
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))  # This is your Project Root
 execution_id = datetime.now().strftime("%Y-%m-%d--%H.%M")
-dataset = "apple2orange"
+dataset = "horse2zebra"
 img_size = 512
+clf_name = "alexnet"
 TF_LOG_DIR = f"logs/{dataset}_clf_{img_size}/{execution_id}"
-checkpoint_path_name = f"checkpoints/inception_{dataset}_{img_size}/{execution_id}/"
+TFDS_PATH = f"{ROOT_DIR}/../tensorflow_datasets"
 
 py.arg('--dataset', default=dataset)
-py.arg('--datasets_dir', default='datasets')
-py.arg('--load_size', type=int, default=img_size+30)  # load image to this size
+py.arg('--datasets_dir', default='tensorflow_datasets')
+py.arg('--load_size', type=int, default=img_size + 30)  # load image to this size
 py.arg('--crop_size', type=int, default=img_size)  # then crop to this size
 py.arg('--batch_size', type=int, default=32)
 py.arg('--epochs', type=int, default=40)
+py.arg('--load_checkpoint', type=str, default=None)
 args = py.args()
 
 # ==============================================================================
@@ -26,28 +35,79 @@ args = py.args()
 # ==============================================================================
 special_normalisation = tf.keras.applications.inception_v3.preprocess_input
 
-A_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainA'), '*.jpg')
-B_img_paths = py.glob(py.join(args.datasets_dir, args.dataset, 'trainB'), '*.jpg')
-A_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testA'), '*.jpg')
-B_img_paths_test = py.glob(py.join(args.datasets_dir, args.dataset, 'testB'), '*.jpg')
+if args.dataset in ["horse2zebra", "apple2orange"]:
+    A_img_paths = py.glob(py.join(TFDS_PATH, args.dataset, 'trainA'), '*.jpg')
+    B_img_paths = py.glob(py.join(TFDS_PATH, args.dataset, 'trainB'), '*.jpg')
+    A_img_paths_test = py.glob(py.join(TFDS_PATH, args.dataset, 'testA'), '*.jpg')
+    B_img_paths_test = py.glob(py.join(TFDS_PATH, args.dataset, 'testB'), '*.jpg')
 
-A_B_dataset, len_dataset = standard_datasets_loading.make_concat_dataset(A_img_paths[200:], B_img_paths[200:], args.batch_size, args.load_size,
+    A_B_dataset, len_dataset = standard_datasets_loading.make_concat_dataset(A_img_paths[200:], B_img_paths[200:],
+                                                                             args.batch_size, args.load_size,
+                                                                             args.crop_size, training=True,
+                                                                             repeat=False,
+                                                                             special_normalisation=special_normalisation)
+
+    A_B_dataset_valid, _ = standard_datasets_loading.make_concat_dataset(A_img_paths[:200], B_img_paths[:200],
+                                                                         args.batch_size, args.load_size,
                                                                          args.crop_size, training=True, repeat=False,
                                                                          special_normalisation=special_normalisation)
 
-A_B_dataset_valid, _ = standard_datasets_loading.make_concat_dataset(A_img_paths[:200], B_img_paths[:200], args.batch_size, args.load_size,
-                                                                     args.crop_size, training=True, repeat=False,
-                                                                     special_normalisation=special_normalisation)
+    A_B_dataset_test, _ = standard_datasets_loading.make_concat_dataset(A_img_paths_test, B_img_paths_test,
+                                                                        args.batch_size,
+                                                                        args.load_size,
+                                                                        args.crop_size, training=True, repeat=False,
+                                                                        special_normalisation=special_normalisation)
+elif args.dataset == "rsna":
+    A_B_dataset, A_B_dataset_valid, A_B_dataset_test, len_dataset_train = get_rsna_ds_for_clf(TFDS_PATH,
+                                                                                              args.batch_size,
+                                                                                              args.crop_size,
+                                                                                              args.load_size,
+                                                                                              special_normalisation,
+                                                                                              channels=1)
+else:  # Mura
+    pass
 
-
-
-A_B_dataset_test, _ = standard_datasets_loading.make_concat_dataset(A_img_paths_test, B_img_paths_test, args.batch_size, args.load_size,
-                                                                    args.crop_size, training=True, repeat=False,
-                                                                    special_normalisation=special_normalisation)
 
 # ==============================================================================
 
-model = Domain2DomainModel(img_shape=(args.crop_size, args.crop_size, 3)).model()
+def evaluate_model(model):
+    print("Evaluating model")
+    y_true = []
+    y_pred = []
+    for i, (x, y) in enumerate(A_B_dataset_test):
+        y_true.append(y.numpy())
+        y_pred.append(model.predict(x).argmax(axis=1))
+    y_true = np.concatenate(y_true)
+    y_pred = np.concatenate(y_pred)
+    print(f"Accuracy: {accuracy_score(y_true, y_pred)}")
+    print(f"Precision: {precision_score(y_true, y_pred)}")
+    print(f"Recall: {recall_score(y_true, y_pred)}")
+    print(f"F1: {f1_score(y_true, y_pred)}")
+
+
+if args.load_checkpoint is not None:
+    checkpoint_path_name = f"checkpoints/{clf_name}_{dataset}/{args.load_checkpoint}/"
+    model = tf.keras.models.load_model(
+        f"{ROOT_DIR}/{checkpoint_path_name}/model", compile=False)
+    metric_auc = tf.keras.metrics.AUC(curve='ROC', multi_label=True, num_labels=2, from_logits=False)
+    metric_f1 = tfa.metrics.F1Score(num_classes=2, threshold=0.5, average='macro')
+    metric_precision = tf.keras.metrics.Precision()
+    metric_recall = tf.keras.metrics.Recall()
+
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+                  loss='categorical_crossentropy',
+                  metrics=["accuracy", metric_auc, metric_f1, metric_precision, metric_recall])
+    result = model.evaluate(A_B_dataset_test)
+    result = dict(zip(model.metrics_names, result))
+    print(json.dumps(result))
+    quit()
+else:
+    if dataset in ["horse2zebra", "apple2orange"]:
+        model = CatsVSDogsModel(img_shape=(args.crop_size, args.crop_size, 3)).model()
+    else:
+        model = Domain2DomainModel(img_shape=(args.crop_size, args.crop_size, 3)).model()
+
+    checkpoint_path_name = f"checkpoints/{clf_name}_{dataset}/{execution_id}/"
 
 my_callbacks = [
     keras.callbacks.ModelCheckpoint(filepath=checkpoint_path_name,
@@ -85,8 +145,8 @@ my_callbacks = [
 metric_auc = tf.keras.metrics.AUC(curve='ROC', multi_label=True, num_labels=2, from_logits=False)
 
 model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-                  loss='categorical_crossentropy',
-                  metrics=["accuracy", metric_auc])
+              loss='categorical_crossentropy',
+              metrics=["accuracy", metric_auc])
 
 # Model Training
 history = model.fit(A_B_dataset,
@@ -99,12 +159,7 @@ history = model.fit(A_B_dataset,
 print("Train History")
 result = model.evaluate(A_B_dataset_test)
 result = dict(zip(model.metrics_names, result))
-result_matrix = [[k, str(w)] for k, w in result.items()]
-for metric, value in zip(model.metrics_names, result):
+for metric, value in result.items():
     print(metric, ": ", value)
 
-print("Result Matrix")
-print(result_matrix)
-print("Result")
-print(result)
 model.save(checkpoint_path_name + 'model')
