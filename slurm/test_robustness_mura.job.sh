7b937eda6a84b0b1f1d083bb4c6239f3d075c60e
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-26 09:01:12 +0100

    Added slurms for robustness.

---
+++ slurm/test_robustness_mura.job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m evaluation/scripts/evaluate_robustness \
+--dataset mura \
+
+
