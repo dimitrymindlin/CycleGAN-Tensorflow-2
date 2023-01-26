8e63e92f71b7774cbf77aa83d85574c14019eb09
dimi9595 <dmitry.mindlin@gmail.com>
2023-01-26 09:06:47 +0100

    Changed path.

--- slurm/test_robustness_mura.job.sh
+++ slurm/test_robustness_mura.job.sh
@@ -3,7 +3,7 @@ source /media/compute/homes/dmindlin/.bashrc
 conda activate medical-decision-support-system
 cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
 
-python3 -m evaluation/scripts/evaluate_robustness \
+python3 -m ./evaluation/scripts/evaluate_robustness \
 --dataset mura \
 
 
