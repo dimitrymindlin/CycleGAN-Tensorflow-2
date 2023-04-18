9d9bafbe746cc75b47c9ebe96be072b2c133bf28
dimi9595 <dmitry.mindlin@gmail.com>
2023-04-18 14:11:26 +0200

    correted a2o clf name.

--- experiment.py
+++ experiment.py
@@ -43,6 +43,7 @@ def setup_args_for_experiment():
             args.clf_ckp_name = "2022-10-12--10.37"
     elif args.dataset == "apple2orange":
         args.clf_ckp_name = "2023-04-17--15.53"  # 2022-09-23--15.18
+        args.clf_name = "simplenet"
         # h2z "2022-06-04--00.00"
     elif args.dataset == "celeba":
         args.crop_size = (218, 178)
@@ -59,8 +60,8 @@ def setup_args_for_experiment():
 py.arg('--dataset', default='celeba', choices=['horse2zebra', 'mura', 'apple2orange', "rsna", "celeba"])
 py.arg('--body_parts', default=["XR_WRIST"])  # Only used in Mura dataset. Body part of x-ray images
 py.arg('--datasets_dir', default='datasets')
-py.arg('--load_size', type=int, default=286)  # load image to this size
-py.arg('--crop_size', type=int, default=256)  # then crop to this size
+py.arg('--load_size', type=int, default=(256, 256))  # load image to this size
+py.arg('--crop_size', type=int, default=(256, 256))  # then crop to this size
 py.arg('--img_channels', type=int, default=3)
 py.arg('--batch_size', type=int, default=1)
 py.arg('--epochs', type=int, default=200)
@@ -73,15 +74,16 @@ py.arg('--cycle_loss_weight', type=float, default=10)
 py.arg('--counterfactual_loss_weight', type=float, default=1)
 py.arg('--identity_loss_weight', type=float, default=1)
 py.arg('--pool_size', type=int, default=50)  # pool size to store fake samples
-py.arg('--attention', type=str, default="gradcam-plus-plus", choices=['gradcam', 'gradcam-plus-plus'])
-py.arg('--clf_name', type=str, default="alexnet", choices=['alexnet', 'inception', 'simplenet'])
+py.arg('--attention', type=str, default="gradcam-plus-plus", choices=['gradcam', 'gradcam-plus-plus', 'occlusion'])
+py.arg('--clf_name', type=str, default="simplenet", choices=['alexnet', 'inception', 'simplenet'])
 py.arg('--clf_ckp_name', type=str,
-       default="2022-06-04--00.00")  # Mura: 2022-06-04--00.05, H2Z: 2022-06-04--00.00 A2O: 2022-09-23--15.18
+       default="2023-04-17--15.53")  # Mura: 2022-06-04--00.05, H2Z: 2022-06-04--00.00 A2O: 2022-09-23--15.18 RSNA: "2022-06-04--00.00 Celeba: 2023-04-16--17.02
 py.arg('--attention_type', type=str, default="attention-gan-original",
        choices=['attention-gan-foreground', 'none', 'attention-gan-original'])
 py.arg('--current_attention_type', type=str, default="none")
 py.arg('--generator', type=str, default="resnet", choices=['resnet', 'unet'])
-py.arg('--discriminator', type=str, default="patch_gan_attention", choices=['classic', 'patch-gan', 'patch_gan_attention'])
+py.arg('--discriminator', type=str, default="patch_gan_attention",
+       choices=['classic', 'patch-gan', 'patch_gan_attention'])
 py.arg('--disc_norm', type=str, default="instance_norm", choices=['instance_norm', 'none', 'batch_norm', 'layer_norm'])
 py.arg('--load_checkpoint', type=str, default=None)
 py.arg('--start_attention_epoch', type=int, default=0)
