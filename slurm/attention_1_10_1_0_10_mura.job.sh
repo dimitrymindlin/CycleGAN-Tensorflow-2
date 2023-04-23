0f985fe7a07f3f3405daf1aa3aebd261b93bfcc4
dimi9595 <dmitry.mindlin@gmail.com>
2023-04-23 23:57:08 +0200

    adapted slurm.

--- slurm/attention_1_10_1_0_10_mura.job.sh
+++ slurm/attention_1_10_1_0_10_mura.job.sh
@@ -8,10 +8,9 @@ python3 -m experiment \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
   --counterfactual_loss_weight 1 \
-  --identity_loss_weight 1 \
+  --identity_loss_weight 0 \
   --generator resnet \
   --clf_ckp_name 2022-06-04--00.05 \
   --discriminator patch_gan_attention \
-  --start_attention_epoch 10 \
-  --load_checkpoint 2022-09-14--13.30 \
+  --start_attention_epoch 10
 
