8b482f0d664ae0027899797db01bfc428634b45c
dimi9595 <dmitry.mindlin@gmail.com>
2023-06-28 17:16:09 +0200

    added slurm

---
+++ slurm/abc-gan.job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /media/compute/homes/dmindlin/.bashrc
+conda activate medical-decision-support-system
+cd /media/compute/homes/dmindlin/CycleGAN-Tensorflow-2
+
+python3 -m train \
+  --dataset mura \
+  --adversarial_loss_weight 1 \
+  --cycle_loss_weight 10 \
+  --counterfactual_loss_weight 0 \
+  --identity_loss_weight 0 \
+  --generator resnet \
+  --clf_name inception \
+  --start_attention_epoch 0 \
+  --discriminator patch_gan_attention \
+  --cyclegan_mode cyclegan
+
