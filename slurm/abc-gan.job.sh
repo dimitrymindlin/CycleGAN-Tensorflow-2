a9163c3eeab8c44c6613a01070d32276d21f258a
dimi9595 <dmitry.mindlin@gmail.com>
2023-06-30 13:26:08 +0200

    changed to ganterfactual mode.

--- slurm/abc-gan.job.sh
+++ slurm/abc-gan.job.sh
@@ -7,10 +7,10 @@ python3 -m train \
   --dataset mura \
   --adversarial_loss_weight 1 \
   --cycle_loss_weight 10 \
-  --counterfactual_loss_weight 0 \
+  --counterfactual_loss_weight 1 \
   --identity_loss_weight 1 \
   --clf_name inception \
   --start_attention_epoch 0 \
   --discriminator default \
-  --cyclegan_mode cyclegan
+  --cyclegan_mode ganterfactual
 
